1375a9fc7ddb718eb233b18c4639fdfbe893bcd6
Mark Hammond <mhammond@skippinet.com.au>
1999-10-06 05:36:00 +0000

    Test overlapped IO - now I have no excuse for letting any more overlapped bugs through (assuming I remember to run this before releasing!)

--- win32/Demos/win32fileDemo.py
+++ win32/Demos/win32fileDemo.py
@@ -1,9 +1,44 @@
 # Test/Demo the native win32 file API for Python.
-import win32file, win32api
+import win32file, win32api, pywintypes, win32event
 import os
+import time
 
 # todo: Add more tests/demos to make this truly useful!
-
+def OverlappedTest():
+	# Create a file in the %TEMP% directory.
+	testName = os.path.join( win32api.GetTempPath(), "win32filetest.dat" )
+	desiredAccess = win32file.GENERIC_WRITE
+	overlapped = pywintypes.OVERLAPPED()
+	evt = win32event.CreateEvent(None, 0, 0, None)
+	overlapped.hEvent = evt
+	# Create the file and write shit-loads of data to it.
+	h = win32file.CreateFile( testName, desiredAccess, 0, None, win32file.CREATE_ALWAYS, 0, 0)
+	chunk_data = "z" * 0x8000
+	num_loops = 512
+	expected_size = num_loops * len(chunk_data)
+	for i in range(num_loops):
+		win32file.WriteFile(h, chunk_data, overlapped)
+		win32event.WaitForSingleObject(overlapped.hEvent, win32event.INFINITE)
+		overlapped.Offset = overlapped.Offset + len(chunk_data)
+	h.Close()
+	# Now read the data back overlapped
+	overlapped = pywintypes.OVERLAPPED()
+	evt = win32event.CreateEvent(None, 0, 0, None)
+	overlapped.hEvent = evt
+	desiredAccess = win32file.GENERIC_READ
+	h = win32file.CreateFile( testName, desiredAccess, 0, None, win32file.OPEN_EXISTING, 0, 0)
+	buffer = win32file.AllocateReadBuffer(0xFFFF)
+	while 1:
+		try:
+			hr, data = win32file.ReadFile(h, buffer, overlapped)
+			win32event.WaitForSingleObject(overlapped.hEvent, win32event.INFINITE)
+			overlapped.Offset = overlapped.Offset + len(data)
+			
+			assert data is buffer, "Unexpected result from ReadFile - should be the same buffer we passed it"
+		except win32api.error:
+			break
+	h.Close()
+# A simple test using normal read/write operations.
 def Test():
 	# Create a file in the %TEMP% directory.
 	testName = os.path.join( win32api.GetTempPath(), "win32filetest.dat" )
@@ -34,7 +69,9 @@ def Test():
 	if os.path.isfile(testName):
 		print "WARNING: After closing the file, it still exists!"
 		
-	print "Successfully performed some basic tests of win32file!"
-		
+
 if __name__=='__main__':
 	Test()
+	OverlappedTest()
+	print "Successfully performed some basic tests of win32file!"
+
