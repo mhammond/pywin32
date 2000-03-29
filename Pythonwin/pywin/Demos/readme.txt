450dce4d094ae36efcbed7509b093f1510fd5093
cvs2hg <devnull@localhost>
2000-03-29 08:08:37 +0000

    fixup commit for tag 'b129f'

--- Pythonwin/pywin/Demos/readme.txt
+++
@@ -1,3 +0,0 @@
-For a good example of all the demos, start Pythonwin, and run the 
-script guidemo.py
-
--- Pythonwin/pywin/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# Old names for bw compat.
-#import pywin.mfc.object; sys.modules['object'] = pywin.mfc.object
-#import pywin.mfc.dialog; sys.modules['pywindialog'] = pywin.mfc.dialog
-#import pywin.mfc.window; sys.modules['window'] = pywin.mfc.window
-#import pywin.mfc.docview; sys.modules['docview'] = pywin.mfc.docview
-
