57d77f4abd99e85cf5429eacf8e785b8fcddd2fb
Mark Hammond <mhammond@skippinet.com.au>
2000-08-10 00:45:46 +0000

    Whole slew of new printer functions from Kelly Kranabetter.

--- win32/src/win32print/win32print.cpp
+++ win32/src/win32print/win32print.cpp
@@ -75,18 +75,206 @@ static PyObject *PyGetPrinter(PyObject *self, PyObject *args)
 	return rc;
 }
 
+
+// @pymethod int|win32print|AddPrinterConnection|Connects to remote printer
+static PyObject *PyAddPrinterConnection(PyObject *self, PyObject *args)
+{
+	char *printer;
+	if (!PyArg_ParseTuple(args, "s:AddPrinterConnection", 
+	          &printer)) // @pyparm string|printer||printer to connect to (eg: \\server\printer).
+		return NULL;
+	if (!AddPrinterConnection(printer))
+		return PyWin_SetAPIError("AddPrinterConnection");
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+
+// @pymethod int|win32print|DeletePrinterConnection|Removes connection to remote printer
+static PyObject *PyDeletePrinterConnection(PyObject *self, PyObject *args)
+{
+	char *printer;
+	if (!PyArg_ParseTuple(args, "s:DeletePrinterConnection", 
+	          &printer)) // @pyparm string|printer||printer to disconnect from (eg: \\server\printer).
+		return NULL;
+	if (!DeletePrinterConnection(printer))
+		return PyWin_SetAPIError("DeletePrinterConnection");
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+// @pymethod int|win32print|GetDefaultPrinter|Returns the default printer.
+static PyObject *PyGetDefaultPrinter(PyObject *self, PyObject *args)
+{
+	char *printer, *s;
+	int printer_size= 100;
+
+	/* Windows < 2000 does not have a GetDefaultPrinter so the default printer
+	   must be retreived from registry */
+
+	if (NULL == (printer= (char *)malloc(printer_size)))
+	{
+		PyErr_SetString(PyExc_MemoryError, "Malloc failed.");
+		return NULL;
+	}
+	if (0 == GetProfileString("Windows", "Device", "", printer, printer_size))
+	{
+		PyErr_SetString(PyExc_RuntimeError, "The default printer was not found.");
+		return NULL;
+	}
+	if (NULL == (s= strchr(printer, ',')))
+	{
+		PyErr_SetString(PyExc_RuntimeError, "The returned printer is malformed.");
+		return NULL;
+	}
+	*s= 0;
+	PyObject *ret= Py_BuildValue("s", printer);
+	free(printer);
+	return ret;
+}
+
+
+// @pymethod int|win32print|SetDefaultPrinter|Sets the default printer.
+static PyObject *PySetDefaultPrinter(PyObject *self, PyObject *args)
+{
+	char *printer, *info, *dprinter;
+	int info_size= 100;
+
+	/* Windows < 2000 does not have a SetDefaultPrinter so the default printer
+	   must be set in the registry */
+
+	if (!PyArg_ParseTuple(args, "s:SetDefaultPrinter", 
+	        &printer)) // @pyparm string|printer||printer to set as default
+		return NULL;
+	
+	if (NULL == (info= (char *)malloc(info_size)))
+	{
+		PyErr_SetString(PyExc_MemoryError, "Malloc failed.");
+		return NULL;
+	}
+	if (0 == GetProfileString("Devices", printer, "", info, info_size))
+	{
+		free(info);
+		PyErr_SetString(PyExc_RuntimeError, "The printer was not found.");
+		return NULL;
+	}
+	if (NULL == (dprinter= (char *)malloc(strlen(printer) + strlen(info) + 3)))
+	{
+		free(info);
+		PyErr_SetString(PyExc_MemoryError, "Malloc failed.");
+		return NULL;
+	}
+	strcpy(dprinter, printer);
+	strcat(dprinter, ",");
+	strcat(dprinter, info);
+	WriteProfileString("Windows", "device", dprinter);
+	SendNotifyMessage(HWND_BROADCAST,WM_SETTINGCHANGE,0,0);
+	free(dprinter);
+	free(info);
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+
+
+// @pymethod int|win32print|EnumPrinters|Enumerates printers, print servers, domains and print providers.
+static PyObject *PyEnumPrinters(PyObject *self, PyObject *args)
+{
+	DWORD flags;
+	DWORD level= 1;
+	BYTE *buf;
+	DWORD bufsize;
+	DWORD bufneeded;
+	DWORD printersreturned;
+	char *name= NULL;
+	DWORD i;	
+
+	if (!PyArg_ParseTuple(args, "i|zi:EnumPrinters", 
+					&flags,   // @pyparm int|flag|| types of printer objects to enumerate (PRINTER_ENUM_*).
+					&name,    // @pyparm string|name|None|name of printer object.
+					&level))  // @pyparm int|level|1|type of printer info structure (only PRINTER_INFO_1 is supported)			      
+		return NULL;
+	if (level != 1)
+	{
+		PyErr_SetString(PyExc_ValueError, "This information level is not supported");
+		return NULL;
+	}
+	EnumPrinters(flags, name, level, NULL, 0, &bufneeded, &printersreturned);
+	if (GetLastError()!=ERROR_INSUFFICIENT_BUFFER)
+		return PyWin_SetAPIError("EnumPrinters");
+	bufsize= bufneeded;
+	if (NULL == (buf= (BYTE *)malloc(bufsize)))
+	{
+		PyErr_SetString(PyExc_MemoryError, "Malloc failed.");
+		return NULL;
+	}
+
+	if (!EnumPrinters(flags, name, level, buf, bufsize, &bufneeded, &printersreturned))
+	{
+		free(buf);
+		return PyWin_SetAPIError("EnumPrinters");
+	}
+
+	PyObject *ret = PyTuple_New(printersreturned);
+	for (i= 0; i < printersreturned; i++)
+	{
+		PRINTER_INFO_1 *info;
+		info= (PRINTER_INFO_1 *)(buf + i * sizeof(PRINTER_INFO_1));
+		PyTuple_SetItem(ret, i, Py_BuildValue("isss", (int)info->Flags, info->pDescription, info->pName, info->pComment));
+	}
+	free(buf);
+	return ret;
+}
+
+
+
+
+
 /* List of functions exported by this module */
 // @module win32print|A module, encapsulating the Windows Win32 API.
 static struct PyMethodDef win32print_functions[] = {
-	{"OpenPrinter",         PyOpenPrinter, 1}, // @pymeth OpenPrinter|Retrieves a handle to a printer.
-	{"GetPrinter",			PyGetPrinter       ,1}, // @pymeth GetPrinter|Retrieves information about a printer
-	{"ClosePrinter",		PyClosePrinter,     1}, // @pymeth ClosePrinter|Closes a handle to a printer.
+	{"OpenPrinter",				PyOpenPrinter, 1}, // @pymeth OpenPrinter|Retrieves a handle to a printer.
+	{"GetPrinter",				PyGetPrinter       ,1}, // @pymeth GetPrinter|Retrieves information about a printer
+	{"ClosePrinter",			PyClosePrinter,     1}, // @pymeth ClosePrinter|Closes a handle to a printer.
+	{"AddPrinterConnection",	PyAddPrinterConnection, 1}, // @pymeth AddPrinterConnection|Connects to a network printer.
+	{"DeletePrinterConnection",	PyDeletePrinterConnection, 1}, // @pymeth DeletePrinterConnection|Disconnects from a network printer.
+	{"EnumPrinters",			PyEnumPrinters, 1}, // @pymeth EnumPrinters|Enumerates printers, print servers, domains and print providers.
+	{"GetDefaultPrinter",		PyGetDefaultPrinter, 1}, // @pymeth GetDefaultPrinter|Returns the default printer.
+	{"SetDefaultPrinter",		PySetDefaultPrinter, 1}, // @pymeth SetDefaultPrinter|Sets the default printer.
 	{ NULL }
 };
+
+
+static void AddConstant(PyObject *dict, char *name, long val)
+{
+  PyObject *nv = PyInt_FromLong(val);
+  PyDict_SetItemString(dict, name, nv );
+  Py_XDECREF(nv);
+}
+
 
 extern "C" __declspec(dllexport) void
 initwin32print(void)
 {
-  PyObject *module;
+  PyObject *module, *dict;
   module = Py_InitModule("win32print", win32print_functions);
+  dict = PyModule_GetDict(module);
+  AddConstant(dict, "PRINTER_INFO_1", 1);
+  AddConstant(dict, "PRINTER_ENUM_LOCAL", PRINTER_ENUM_LOCAL);
+  AddConstant(dict, "PRINTER_ENUM_NAME", PRINTER_ENUM_NAME);
+  AddConstant(dict, "PRINTER_ENUM_SHARED", PRINTER_ENUM_SHARED);
+  AddConstant(dict, "PRINTER_ENUM_DEFAULT", PRINTER_ENUM_DEFAULT);
+  AddConstant(dict, "PRINTER_ENUM_CONNECTIONS", PRINTER_ENUM_CONNECTIONS);
+  AddConstant(dict, "PRINTER_ENUM_NETWORK", PRINTER_ENUM_NETWORK);
+  AddConstant(dict, "PRINTER_ENUM_REMOTE", PRINTER_ENUM_REMOTE);
+  AddConstant(dict, "PRINTER_ENUM_EXPAND", PRINTER_ENUM_EXPAND);
+  AddConstant(dict, "PRINTER_ENUM_CONTAINER", PRINTER_ENUM_CONTAINER);
+  AddConstant(dict, "PRINTER_ENUM_ICON1", PRINTER_ENUM_ICON1);
+  AddConstant(dict, "PRINTER_ENUM_ICON2", PRINTER_ENUM_ICON2);
+  AddConstant(dict, "PRINTER_ENUM_ICON3", PRINTER_ENUM_ICON3);
+  AddConstant(dict, "PRINTER_ENUM_ICON4", PRINTER_ENUM_ICON4);
+  AddConstant(dict, "PRINTER_ENUM_ICON5", PRINTER_ENUM_ICON5);
+  AddConstant(dict, "PRINTER_ENUM_ICON6", PRINTER_ENUM_ICON6);
+  AddConstant(dict, "PRINTER_ENUM_ICON7", PRINTER_ENUM_ICON7);
+  AddConstant(dict, "PRINTER_ENUM_ICON8", PRINTER_ENUM_ICON8);
 }
