45b72d994935946e288350efa7f34fac06a57662
Mark Hammond <mhammond@skippinet.com.au>
2001-02-09 07:25:57 +0000

    Add a few new tests.

--- win32/Demos/win32wnet/testwnet.py
+++ win32/Demos/win32wnet/testwnet.py
@@ -1,68 +1,74 @@
 import win32wnet
 import sys
 from winnetwk import *
+import os
 
-g = win32wnet.NETRESOURCE()
+possible_shares = []
 
-g.lpProvider = 'ProviderName'
-g.lpLocalName = 'LocalName'
-g.lpRemoteName ='RemoteName'
-g.lpComment = 'Commentlineis here'
+def _doDumpHandle(handle, level = 0):
+		indent = " " * level
+		while 1:
+			items = win32wnet.WNetEnumResource(handle, 0)
+			if len(items)==0:
+				break
+			for item in items:
+				try:
+					if item.dwDisplayType == RESOURCEDISPLAYTYPE_SHARE:
+						print indent + "Have share with name:", item.lpRemoteName
+						possible_shares.append(item)
+					elif item.dwDisplayType == RESOURCEDISPLAYTYPE_GENERIC:
+						print indent + "Have generic resource with name:", item.lpRemoteName
+					else:
+						# Try generic!
+						print indent + "Enumerating " + item.lpRemoteName,
+						k = win32wnet.WNetOpenEnum(RESOURCE_GLOBALNET, RESOURCETYPE_ANY,0,item)
+						print
+						_doDumpHandle(k, level + 1)
+						win32wnet.WNetCloseEnum(k) # could do k.Close(), but this is a good test!
+				except win32wnet.error, details:
+					print indent + "Couldn't enumerate this resource: " + details[2]
 
-print 'start script comment'
-print 'by attribute'
+def TestOpenEnum():
+	print "Enumerating all resources on the network - this may take some time..."
+	handle = win32wnet.WNetOpenEnum(RESOURCE_GLOBALNET,RESOURCETYPE_ANY,0,None)
 
-print g.lpLocalName
-print g.lpRemoteName
-print g.lpProvider
-print g.lpComment
+	try:
+		_doDumpHandle(handle)
+	finally:
+		handle.Close()
+	print "Finished dumping all resources."
 
-print ''
+def TestConnection():
+	if len(possible_shares)==0:
+		print "Couldn't find any potential shares to connect to"
+		return
+	localName = "Z:" # need better way!
+	for share in possible_shares:
+		print "Attempting connection of", localName, "to", share.lpRemoteName
+		try:
+			win32wnet.WNetAddConnection2(share.dwType, localName, share.lpRemoteName)
+		except win32wnet.error, details:
+			print "Couldn't connect: " + details[2]
+			continue
+		# Have a connection.
+		try:
+			fname = os.path.join(localName + "\\", os.listdir(localName + "\\")[0])
+			try:
+				print "Universal name of '%s' is '%s'" % (fname, win32wnet.WNetGetUniversalName(fname))
+			except win32wnet.error, details:
+				print "Couldn't get universal name of '%s': %s" % (fname, details[2])
+			print "User name for this connection is", win32wnet.WNetGetUser(localName)
+		finally:
+			win32wnet.WNetCancelConnection2(localName, 0, 0)
+			# Only do the first share that succeeds.
+			break
 
-try:
-	retval = win32wnet.WNetOpenEnum(RESOURCE_GLOBALNET,RESOURCETYPE_ANY,0,None)
-except:
-	print 'exception taken on OpenEnum'
-	print g
-	raise
+def TestGetUser():
+	u = win32wnet.WNetGetUser()
+	print "Current global user is", `u`
+	if u != win32wnet.WNetGetUser(None):
+		raise RuntimeError, "Default value didnt seem to work!"
 
-print 'retvalue valid'
-print retval
-
-try:
-	root = win32wnet.WNetEnumResource(retval, 0xffffffff)
-	win32wnet.WNetCloseEnum(retval)
-	
-except:
-	print 'exception taken on EnumResource'
-	raise
-	
-
-h = win32wnet.NETRESOURCE()
-h.lpRemoteName ='\\\\171.69.164.176'
-#h.setstring('lpProvider', 'Microsoft Windows Network')
-h.dwUsage = RESOURCEUSAGE_CONTAINER
-h.dwScope = RESOURCE_GLOBALNET
-h.dwType = RESOURCETYPE_ANY
-h.dwDisplayType = RESOURCEDISPLAYTYPE_GENERIC
-
-
-
-
-
-try:
-	k = win32wnet.WNetOpenEnum(RESOURCE_GLOBALNET, RESOURCETYPE_ANY,0,h)
-	print 'back from openenum'
-	sub1 = win32wnet.WNetEnumResource(k, 2)
-	sub2 = win32wnet.WNetEnumResource(k, 2)
-	win32wnet.WNetCloseEnum(k)
-	print sub1
-	print sub2
-	
-	
-except:
-	print 'exception taken on sub 1 enumeration'
-	raise
-
-
-print 'End of Program'	
+TestGetUser()
+TestOpenEnum()
+TestConnection()
