264833501313e7e91f3f12bb22653c21bb65ea18
Mark Hammond <mhammond@skippinet.com.au>
2001-05-27 08:39:45 +0000

    Reapply revision 1.4 changes with whitespace corrected (and one extra autoduck annotation)

--- win32/src/win32print/win32print.cpp
+++ win32/src/win32print/win32print.cpp
@@ -17,6 +17,7 @@ generates Windows .hlp files.
 #include "Python.h"
 #include "PyWinTypes.h"
 #include "PyWinObjects.h"
+#include <stdarg.h>
 
 
 // Printer stuff.
@@ -76,7 +77,7 @@ static PyObject *PyGetPrinter(PyObject *self, PyObject *args)
 }
 
 
-// @pymethod int|win32print|AddPrinterConnection|Connects to remote printer
+// @pymethod None|win32print|AddPrinterConnection|Connects to remote printer
 static PyObject *PyAddPrinterConnection(PyObject *self, PyObject *args)
 {
 	char *printer;
@@ -90,7 +91,7 @@ static PyObject *PyAddPrinterConnection(PyObject *self, PyObject *args)
 }
 
 
-// @pymethod int|win32print|DeletePrinterConnection|Removes connection to remote printer
+// @pymethod None|win32print|DeletePrinterConnection|Removes connection to remote printer
 static PyObject *PyDeletePrinterConnection(PyObject *self, PyObject *args)
 {
 	char *printer;
@@ -103,14 +104,14 @@ static PyObject *PyDeletePrinterConnection(PyObject *self, PyObject *args)
 	return Py_None;
 }
 
-// @pymethod int|win32print|GetDefaultPrinter|Returns the default printer.
+// @pymethod string|win32print|GetDefaultPrinter|Returns the default printer.
 static PyObject *PyGetDefaultPrinter(PyObject *self, PyObject *args)
 {
 	char *printer, *s;
 	int printer_size= 100;
 
 	/* Windows < 2000 does not have a GetDefaultPrinter so the default printer
-	   must be retreived from registry */
+	   must be retrieved from registry */
 
 	if (NULL == (printer= (char *)malloc(printer_size)))
 	{
@@ -134,7 +135,7 @@ static PyObject *PyGetDefaultPrinter(PyObject *self, PyObject *args)
 }
 
 
-// @pymethod int|win32print|SetDefaultPrinter|Sets the default printer.
+// @pymethod None|win32print|SetDefaultPrinter|Sets the default printer.
 static PyObject *PySetDefaultPrinter(PyObject *self, PyObject *args)
 {
 	char *printer, *info, *dprinter;
@@ -177,7 +178,7 @@ static PyObject *PySetDefaultPrinter(PyObject *self, PyObject *args)
 
 
 
-// @pymethod int|win32print|EnumPrinters|Enumerates printers, print servers, domains and print providers.
+// @pymethod tuple|win32print|EnumPrinters|Enumerates printers, print servers, domains and print providers.
 static PyObject *PyEnumPrinters(PyObject *self, PyObject *args)
 {
 	DWORD flags;
@@ -216,6 +217,8 @@ static PyObject *PyEnumPrinters(PyObject *self, PyObject *args)
 	}
 
 	PyObject *ret = PyTuple_New(printersreturned);
+	        // rdesc The result is a tuple of tuples; one for each printer enumerated.
+	        // Each individual element is a tuple of (flags, description, name, comment)
 	for (i= 0; i < printersreturned; i++)
 	{
 		PRINTER_INFO_1 *info;
@@ -227,6 +230,325 @@ static PyObject *PyEnumPrinters(PyObject *self, PyObject *args)
 }
 
 
+// @pymethod int|win32print|StartDocPrinter|Notifies the print spooler that a document is to be spooled for printing. To be used before using WritePrinter. Returns the Jobid of the started job.
+static PyObject *PyStartDocPrinter(PyObject *self, PyObject *args)
+{
+	HANDLE hprinter;
+	DWORD level= 1;
+	char *pDocName, *pOutputFile, *pDatatype;
+	DOC_INFO_1 info;
+	DWORD JobID;
+
+	if (!PyArg_ParseTuple(args, "ii(szz):StartDocPrinter",
+	            &hprinter, // @pyparm int|hprinter||handle to printer (from OpenPrinter)
+	            &level,     // @pyparm int|level|1|type of docinfo structure (only docinfo level 1 supported)
+	            &pDocName, &pOutputFile, &pDatatype // @pyparm (string, string, string)|(DocName, OutputFile, DataType)||Sequence of document name, output file, data type
+	        ))
+		return NULL;
+
+	if (level != 1)
+	{
+		PyErr_SetString(PyExc_ValueError, "This information level is not supported");
+		return NULL;
+	}
+
+	info.pDocName= pDocName;
+	info.pOutputFile= pOutputFile;
+	info.pDatatype= pDatatype;
+
+	if (0 == (JobID= StartDocPrinter(hprinter, level, (LPBYTE)&info)))
+		return PyWin_SetAPIError("StartDocPrinter");
+
+	PyObject *ret= Py_BuildValue("i", JobID);
+	return ret;
+}
+
+
+// @pymethod None|win32print|EndDocPrinter|The EndDocPrinter function ends a print job for the specified printer. To be used after using WritePrinter.
+static PyObject *PyEndDocPrinter(PyObject *self, PyObject *args)
+{
+	HANDLE hprinter;
+
+	if (!PyArg_ParseTuple(args, "i:EndDocPrinter",
+	            &hprinter  // @pyparm int|hprinter||handle to printer (from OpenPrinter)
+	        ))
+		return NULL;
+
+	if (!EndDocPrinter(hprinter))
+		return PyWin_SetAPIError("EndDocPrinter");
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+
+// @pymethod int|win32print|WritePrinter|Copies the specified bytes to the specified printer. Suitable for copying raw Postscript or HPGL files to a printer. StartDocPrinter and EndDocPrinter should be called before and after. Returns number of bytes written to printer.
+static PyObject *PyWritePrinter(PyObject *self, PyObject *args)
+{
+	HANDLE hprinter;
+	LPVOID buf;
+	DWORD buf_size;
+	DWORD bufwritten_size;
+
+	if (!PyArg_ParseTuple(args, "is#:WritePrinter",
+	            &hprinter,  // @pyparm int|hprinter||Handle to printer (from OpenPrinter)
+	            &buf,       // @pyparm string|buf||String to send to printer. Embedded NULL bytes are allowed.
+	            &buf_size
+	        ))
+		return NULL;
+
+	if (!WritePrinter(hprinter, buf, buf_size, &bufwritten_size))
+		return PyWin_SetAPIError("WritePrinter");
+
+	PyObject *ret= Py_BuildValue("i", bufwritten_size);
+	return ret;
+}
+
+
+// convert a job structure to python. only works for level 1
+PyObject *JobtoPy(DWORD level, LPBYTE buf)
+{
+	if (level != 1)
+		return NULL;
+
+	JOB_INFO_1 *job1= (JOB_INFO_1 *)buf;
+	SYSTEMTIME localSubmitted;
+
+	SystemTimeToTzSpecificLocalTime(NULL, &(job1->Submitted), &localSubmitted);
+	PyObject *pylocalsubmitted= new PyTime(localSubmitted);
+	PyObject *ret= Py_BuildValue("{s:i, s:s, s:s, s:s, s:s, s:s, s:s, s:i, s:i, s:i, s:i, s:i, s:O}",
+	        "JobID", job1->JobId,
+	        "pPrinterName", job1->pPrinterName,
+	        "pMachineName", job1->pMachineName,
+	        "pUserName", job1->pUserName,
+	        "pDocument", job1->pDocument,
+	        "pDatatype", job1->pDatatype,
+	        "pStatus", job1->pStatus,
+	        "Status", job1->Status,
+	        "Priority", job1->Priority,
+	        "Position", job1->Position,
+	        "TotalPages", job1->TotalPages,
+	        "PagesPrinted", job1->PagesPrinted,
+	        "Submitted", pylocalsubmitted);
+	Py_XDECREF(pylocalsubmitted);
+	return ret;
+}
+
+// @pymethod tuple|win32print|EnumJobs|Enumerates print jobs on specified printer.
+static PyObject *PyEnumJobs(PyObject *self, PyObject *args)
+{
+	HANDLE hprinter;
+	DWORD firstjob;
+	DWORD nojobs;
+	DWORD level= 1;
+	LPBYTE buf;
+	DWORD buf_size;
+	DWORD bufneeded_size;
+	DWORD jobsreturned;
+
+	if (!PyArg_ParseTuple(args, "iiii:EnumJobs",
+	          &hprinter,   // @pyparm int|hPrinter||Handle of printer.
+	          &firstjob,   // @pyparm int|FirstJob||location of first job in print queue to enumerate.
+	          &nojobs,     // @pyparm int|NoJobs||Number of jobs to enumerate.
+	          &level       // @pyparm int|Level|1|Level of information to return (only JOB_INFO_1 is supported).
+	          ))
+		return NULL;
+	if (level != 1)
+	{
+		PyErr_SetString(PyExc_ValueError, "This information level is not supported");
+		return NULL;
+	}
+	EnumJobs(hprinter, firstjob, nojobs, level, NULL, 0, &bufneeded_size, &jobsreturned);
+	if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
+		return PyWin_SetAPIError("EnumJobs");
+	buf_size= bufneeded_size;
+	if (NULL == (buf= (LPBYTE)malloc(buf_size)))
+	{
+		PyErr_SetString(PyExc_MemoryError, "Malloc failed.");
+		return NULL;
+	}
+	if (!EnumJobs(hprinter, firstjob, nojobs, level, buf, buf_size, &bufneeded_size, &jobsreturned))
+	{
+		free(buf);
+		return PyWin_SetAPIError("EnumJobs");
+	}
+
+	DWORD i;
+	PyObject *ret = PyTuple_New(jobsreturned);
+	for (i= 0; i < jobsreturned; i++)
+	{
+		PyTuple_SetItem(ret, i, JobtoPy(1, (buf + i * sizeof(JOB_INFO_1))));
+	}
+	free(buf);
+	return ret;
+}
+
+
+// @pymethod dictionary|win32print|GetJob|Returns dictionary of information about a specified print job.
+static PyObject *PyGetJob(PyObject *self, PyObject *args)
+{
+	HANDLE hprinter;
+	DWORD jobid;
+	DWORD level= 1;
+	JOB_INFO_1 *buf;
+	DWORD buf_size;
+	DWORD bufneeded_size;
+
+	if (!PyArg_ParseTuple(args, "ii|i:GetJob",
+	          &hprinter,// @pyparm int|hPrinter||Handle of printer.
+	          &jobid,   // @pyparm int|JobID||Job Identifier.
+	          &level   // @pyparm int|Level|1|Level of information to return (only JOB_INFO_1 is supported).
+	          ))
+		return NULL;
+	if (level != 1)
+	{
+		PyErr_SetString(PyExc_ValueError, "This information level is not supported");
+		return NULL;
+	}
+	GetJob(hprinter, jobid, level, NULL, 0, &bufneeded_size);
+	if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
+		return PyWin_SetAPIError("GetJob");
+	buf_size= bufneeded_size;
+	if (NULL == (buf= (JOB_INFO_1 *)malloc(buf_size)))
+	{
+		PyErr_SetString(PyExc_MemoryError, "Malloc failed.");
+		return NULL;
+	}
+	if (!GetJob(hprinter, jobid, level, (LPBYTE)buf, buf_size, &bufneeded_size))
+	{
+		free(buf);
+		return PyWin_SetAPIError("GetJob");
+	}
+	PyObject *ret= JobtoPy(1, (LPBYTE)buf);
+	free(buf);
+	return ret;
+}
+
+
+// Convert a python dictionary to a job structure. Only works for level 1
+// There has got to be an easier way to do this...
+// Returned buffer must be freed.
+LPBYTE PytoJob(DWORD level, PyObject *pyjobinfo)
+{
+	PyObject* temp;
+	char *err= NULL;
+
+	if (level != 1)
+		return NULL;
+
+	JOB_INFO_1 *job1;
+	if (!PyDict_Check (pyjobinfo))
+	{
+		PyErr_SetString(PyExc_ValueError, "JOB_INFO must be a dictionary.");
+		return NULL;
+	}
+	if (NULL == (job1= (JOB_INFO_1 *)malloc(sizeof(JOB_INFO_1))))
+	{
+		PyErr_SetString(PyExc_MemoryError, "Malloc failed.");
+		return NULL;
+	}
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "JobID")) && PyInt_Check(temp))
+		job1->JobId= PyInt_AsLong(temp);
+	else
+		err= "JobID invalid";
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "pPrinterName")) && PyString_Check(temp))
+		job1->pPrinterName= PyString_AsString(temp);
+	else
+		err= "pPrinterName invalid";
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "pMachineName")) && PyString_Check(temp))
+		job1->pMachineName= PyString_AsString(temp);
+	else
+		err= "pMachineName invalid";
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "pUserName")) && PyString_Check(temp))
+		job1->pUserName= PyString_AsString(temp);
+	else
+		err= "pUsername invalid";
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "pDocument")) && PyString_Check(temp))
+		job1->pDocument= PyString_AsString(temp);
+	else
+		err= "pDocument invalid";
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "pDatatype")) && PyString_Check(temp))
+		job1->pDatatype= PyString_AsString(temp);
+	else
+		err= "pDatatype invalid";
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "pStatus")))
+		if (PyString_Check(temp))
+			job1->pStatus= PyString_AsString(temp);
+		else
+			job1->pStatus= NULL;
+	else
+		err= "pStatus invalid";
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "Status")) && PyInt_Check(temp))
+		job1->Status= PyInt_AsLong(temp);
+	else
+		err= "Status invalid";
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "Priority")) && PyInt_Check(temp))
+		job1->Priority= PyInt_AsLong (temp);
+	else
+		err= "Priority invalid";
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "Position")) && PyInt_Check(temp))
+		job1->Position= PyInt_AsLong (temp);
+	else
+		err= "Position invalid";
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "TotalPages")) && PyInt_Check(temp))
+		job1->TotalPages= PyInt_AsLong (temp);
+	else
+		err= "TotalPages invalid";
+	if (NULL != (temp= PyDict_GetItemString(pyjobinfo, "PagesPrinted")) && PyInt_Check(temp))
+		job1->PagesPrinted= PyInt_AsLong(temp);
+	else
+		err= "PagesPrinted invalid";
+	if (err != NULL)
+	{
+		free(job1);
+		PyErr_SetString(PyExc_ValueError, err);
+		return NULL;
+	}
+	return (LPBYTE)job1;
+}
+
+
+// @pymethod None|win32print|SetJob|Pause, cancel, resume, set priority levels on a print job.
+static PyObject *PySetJob(PyObject *self, PyObject *args)
+{
+	HANDLE hprinter;
+	DWORD jobid;
+	DWORD level= 1;
+	PyObject *pyjobinfo;
+	DWORD command;
+	LPBYTE buf;
+
+	if (!PyArg_ParseTuple(args, "iiiOi:GetJob",
+	    &hprinter,// @pyparm int|hPrinter||Handle of printer.
+	    &jobid,   // @pyparm int|JobID||Job Identifier.
+	    &level,   // @pyparm int|Level|1|Level of information to return (only 0 and JOB_INFO_1 are supported).
+	    &pyjobinfo, // @pyparm dict|JobInfo||JOB_INFO_1 Dictionary (can be None if Level is 0). Position should be JOB_POSITION_UNSPECIFIED.
+	    &command  // @pyparm int|Command||Job command value (JOB_CONTROL_*).
+	    ))
+		return NULL;
+	if (level != 1 && level != 0)
+	{
+		PyErr_SetString(PyExc_ValueError, "This information level is not supported");
+		return NULL;
+	}
+	if (pyjobinfo == Py_None)
+		buf= NULL;
+	else
+	{
+		if (NULL == (buf= PytoJob(1, pyjobinfo)))
+			return NULL;
+	}
+	if (!SetJob(hprinter, jobid, level, buf, command))
+	{
+		if (buf)
+			free(buf);
+		return PyWin_SetAPIError("SetJob");
+	}
+	if (buf)
+		free(buf);
+	Py_INCREF(Py_None);
+	return Py_None;
+}
 
 
 
@@ -241,6 +563,12 @@ static struct PyMethodDef win32print_functions[] = {
 	{"EnumPrinters",			PyEnumPrinters, 1}, // @pymeth EnumPrinters|Enumerates printers, print servers, domains and print providers.
 	{"GetDefaultPrinter",		PyGetDefaultPrinter, 1}, // @pymeth GetDefaultPrinter|Returns the default printer.
 	{"SetDefaultPrinter",		PySetDefaultPrinter, 1}, // @pymeth SetDefaultPrinter|Sets the default printer.
+	{"StartDocPrinter",     PyStartDocPrinter, 1},   // @pymeth StartDocPrinter|Notifies the print spooler that a document is to be spooled for printing. Returns the Jobid of the started job.
+	{"EndDocPrinter",     PyEndDocPrinter, 1},   // @pymeth EndDocPrinter|The EndDocPrinter function ends a print job for the specified printer.
+	{"WritePrinter",      PyWritePrinter, 1},   // @pymeth WritePrinter|Copies the specified bytes to the specified printer. StartDocPrinter and EndDocPrinter should be called before and after. Returns number of bytes written to printer.
+	{"EnumJobs",        PyEnumJobs, 1},   // @pymeth EnumJobs|Enumerates print jobs on specified printer.
+	{"GetJob",          PyGetJob, 1},   // @pymeth GetJob|Returns dictionary of information about a specified print job.
+	{"SetJob",          PySetJob, 1},   // @pymeth SetJob|Pause, cancel, resume, set priority levels on a print job.
 	{ NULL }
 };
 
@@ -279,4 +607,21 @@ initwin32print(void)
   AddConstant(dict, "PRINTER_ENUM_ICON6", PRINTER_ENUM_ICON6);
   AddConstant(dict, "PRINTER_ENUM_ICON7", PRINTER_ENUM_ICON7);
   AddConstant(dict, "PRINTER_ENUM_ICON8", PRINTER_ENUM_ICON8);
+  AddConstant(dict, "JOB_STATUS_DELETING", JOB_STATUS_DELETING);
+  AddConstant(dict, "JOB_STATUS_ERROR", JOB_STATUS_ERROR);
+  AddConstant(dict, "JOB_STATUS_OFFLINE", JOB_STATUS_OFFLINE);
+  AddConstant(dict, "JOB_STATUS_PAPEROUT", JOB_STATUS_PAPEROUT);
+  AddConstant(dict, "JOB_STATUS_PAUSED", JOB_STATUS_PAUSED);
+  AddConstant(dict, "JOB_STATUS_PRINTED", JOB_STATUS_PRINTED);
+  AddConstant(dict, "JOB_STATUS_PRINTING", JOB_STATUS_PRINTING);
+  AddConstant(dict, "JOB_STATUS_SPOOLING", JOB_STATUS_SPOOLING);
+  AddConstant(dict, "MIN_PRIORITY", MIN_PRIORITY);
+  AddConstant(dict, "MAX_PRIORITY", MAX_PRIORITY);
+  AddConstant(dict, "DEF_PRIORITY", DEF_PRIORITY);
+  AddConstant(dict, "JOB_INFO_1", 1);
+  AddConstant(dict, "JOB_CONTROL_CANCEL", JOB_CONTROL_CANCEL);
+  AddConstant(dict, "JOB_CONTROL_PAUSE", JOB_CONTROL_PAUSE);
+  AddConstant(dict, "JOB_CONTROL_RESTART", JOB_CONTROL_RESTART);
+  AddConstant(dict, "JOB_CONTROL_RESUME", JOB_CONTROL_RESUME);
+  AddConstant(dict, "JOB_POSITION_UNSPECIFIED", JOB_POSITION_UNSPECIFIED);
 }
