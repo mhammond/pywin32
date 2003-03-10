4ac1f14e57be3440ceb04b16fe6f283bf1a93d51
Mark Hammond <mhammond@skippinet.com.au>
2003-03-10 01:15:22 +0000

    XP seems to behave differently when requesting buffer size.

--- win32/src/win32pdhmodule.cpp
+++ win32/src/win32pdhmodule.cpp
@@ -252,7 +252,9 @@ static PyObject *PyEnumObjectItems(PyObject *self, PyObject *args)
         flags); 
 	Py_END_ALLOW_THREADS
 
-	if (pdhStatus != ERROR_SUCCESS)  {
+	// it appears NT/2k will return 0, while XP will return
+	// PDH_MORE_DATA
+	if (pdhStatus != ERROR_SUCCESS && pdhStatus != PDH_MORE_DATA)  {
 		PyWinObject_FreeTCHAR(strMachine);
 		PyWinObject_FreeTCHAR(strObject);
 		return PyWin_SetAPIError("EnumObjectItems for buffer size", pdhStatus);
