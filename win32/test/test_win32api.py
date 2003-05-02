8debc79678a94afc7e07dda7b1282404ca1239d0
Mark Hammond <mhammond@skippinet.com.au>
2003-05-02 00:06:06 +0000

    Demonstrate another bug yet to be fixed (but in SF)

--- win32/test/test_win32api.py
+++ win32/test/test_win32api.py
@@ -22,5 +22,21 @@ class TimeZone(unittest.TestCase):
         print "Time zone in effect is", tz_str.encode()
         print "Next timezone change happens at", tz_time.Format()
 
+class Registry(unittest.TestCase):
+    def test1(self):
+        # This used to leave a stale exception behind.
+        key_name = r'PythonTestHarness\Whatever'
+        def reg_operation():
+            hkey = win32api.RegCreateKey(win32con.HKEY_CURRENT_USER, key_name)
+            x = 3/0 # or a statement like: raise 'error'
+        # do the test
+        try:
+            try:
+                reg_operation()
+            except:
+                1/0 # Force exception
+        finally:
+            win32api.RegDeleteKey(win32con.HKEY_CURRENT_USER, key_name)
+
 if __name__ == '__main__':
     unittest.main()
