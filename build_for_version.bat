f1d027ce24f733c4ced14e577a44136fa7f09b3a
Mark Hammond <mhammond@skippinet.com.au>
2003-07-27 11:55:53 +0000

    Shouldn't rely on .py associations being setup.

--- build_for_version.bat
+++ build_for_version.bat
@@ -1,7 +1,7 @@
 @echo off
 call removebuilt.bat
 if errorlevel 1 goto failed
-set_for_version.py %1
+python set_for_version.py %1
 if errorlevel 1 goto failed
 msdev "Python and Extensions.dsw" /MAKE ALL
 if errorlevel 1 goto failed
