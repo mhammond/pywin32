db10e887ad0cec5f1056bf7c3252887fcc0bf1a7
Mark Hammond <mhammond@skippinet.com.au>
2003-10-23 05:17:04 +0000

    Make a unittest test.

--- win32/test/test_security.py
+++ win32/test/test_security.py
@@ -1,27 +1,34 @@
+import sys, os
+import unittest
+
 import win32api, win32con, win32security
 
-pwr_sid=win32security.LookupAccountName('','Power Users')[0]
-admin_sid=win32security.LookupAccountName('','Administrator')[0]
+class SecurityTests(unittest.TestCase):
+    def setUp(self):
+        self.pwr_sid=win32security.LookupAccountName('','Power Users')[0]
+        self.admin_sid=win32security.LookupAccountName('','Administrator')[0]
 
-sd=win32security.SECURITY_DESCRIPTOR()
-for x in xrange(0,100000):
-    sd.SetSecurityDescriptorOwner(admin_sid,0)
+    def tearDown(self):
+        pass
 
-sd=win32security.SECURITY_DESCRIPTOR()
-for x in xrange(0,100000):
-    sd.SetSecurityDescriptorGroup(pwr_sid,0)
+    def testMemory(self):
+        pwr_sid = self.pwr_sid
+        admin_sid = self.admin_sid
+        sd1=win32security.SECURITY_DESCRIPTOR()
+        sd2=win32security.SECURITY_DESCRIPTOR()
+        sd3=win32security.SECURITY_DESCRIPTOR()
+        dacl=win32security.ACL()
+        dacl.AddAccessAllowedAce(win32security.ACL_REVISION,win32con.GENERIC_READ,pwr_sid)
+        dacl.AddAccessAllowedAce(win32security.ACL_REVISION,win32con.GENERIC_ALL,admin_sid)
+        sd4=win32security.SECURITY_DESCRIPTOR()
+        sacl=win32security.ACL()
+        sacl.AddAuditAccessAce(win32security.ACL_REVISION,win32con.DELETE,admin_sid,1,1)
+        sacl.AddAuditAccessAce(win32security.ACL_REVISION,win32con.GENERIC_ALL,pwr_sid,1,1)
+        for x in xrange(0,200000):
+            sd1.SetSecurityDescriptorOwner(admin_sid,0)
+            sd2.SetSecurityDescriptorGroup(pwr_sid,0)
+            sd3.SetSecurityDescriptorDacl(1,dacl,0)
+            sd4.SetSecurityDescriptorSacl(1,sacl,0)
 
-sd=win32security.SECURITY_DESCRIPTOR()
-dacl=win32security.ACL()
-dacl.AddAccessAllowedAce(win32security.ACL_REVISION,win32con.GENERIC_READ,pwr_sid)
-dacl.AddAccessAllowedAce(win32security.ACL_REVISION,win32con.GENERIC_ALL,admin_sid)
-for x in xrange(0,1000000):
-    sd.SetSecurityDescriptorDacl(1,dacl,0)
-
-sd=win32security.SECURITY_DESCRIPTOR()
-sacl=win32security.ACL()
-sacl.AddAuditAccessAce(win32security.ACL_REVISION,win32con.DELETE,admin_sid,1,1)
-sacl.AddAuditAccessAce(win32security.ACL_REVISION,win32con.GENERIC_ALL,pwr_sid,1,1)
-for x in xrange(0,1000000):
-    sd.SetSecurityDescriptorSacl(1,sacl,0)
-
+if __name__=='__main__':
+    unittest.main()
