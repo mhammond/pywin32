94f2fbc87670ab914665ad033cd7c980eee93d89
Mark Hammond <mhammond@skippinet.com.au>
2003-10-26 03:52:56 +0000

    Look for pywintypes.dll in the sys.prefix directory - that is where it goes for non-admin users, and LoadLibrary will fail to locate it (as that is not on our path)

--- win32/Lib/pywintypes.py
+++ win32/Lib/pywintypes.py
@@ -1,10 +1,19 @@
 # Magic utility that "redirects" to pywintypesxx.dll
 
 def __import(modname):
-    import win32api, imp, sys, os
-    suffix = ""
-    if win32api.__file__.endswith("_d.pyd")>0:
-        suffix = "_d"
+    # *sigh* - non-admin installs will not have pywintypesxx.dll in the 
+    # system directory, so 'import win32api' will fail looking
+    # for pywintypes - the exact DLL we are trying to load!
+    # So if it exists in sys.prefix, then we try and load it from
+    # there, as that way we can avoid the win32api import
+    import imp, sys, os
+    # See if this is a debug build.
+    for suffix_item in imp.get_suffixes():
+        if suffix_item[0]=='_d.pyd':
+            suffix = '_d'
+            break
+    else:
+        suffix = ""
     filename = "%s%d%d%s.dll" % (modname, sys.version_info[0], sys.version_info[1], suffix)
     if hasattr(sys, "frozen"):
         # If we are running from a frozen program (py2exe, McMillan, freeze)
@@ -17,16 +26,19 @@ def __import(modname):
             raise ImportError, "Module '%s' isn't in frozen sys.path directories" % modname
         h = None
     else:
-        # Normal Python needs these files in a directory somewhere on
-        # %PATH%, so let Windows search it out for us
-        h = win32api.LoadLibrary(filename)
-        found = win32api.GetModuleFileName(h)
+        if os.path.isfile(os.path.join(sys.prefix, filename)):
+            found = os.path.join(sys.prefix, filename)
+        else:
+            # We could still avoid win32api here, but...
+            import win32api
+            # Normal Python needs these files in a directory somewhere on
+            # %PATH%, so let Windows search it out for us
+            h = win32api.LoadLibrary(filename)
+            found = win32api.GetModuleFileName(h)
     # Python can load the module
     mod = imp.load_module(modname, None, found, ('.dll', 'rb', imp.C_EXTENSION))
     # and fill our namespace with it.
     globals().update(mod.__dict__)
-    if h is not None:
-        win32api.FreeLibrary(h)
 
 __import("pywintypes")
 del __import
