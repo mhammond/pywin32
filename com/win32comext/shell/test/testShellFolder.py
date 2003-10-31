ceaad2c1dc75029dbb4516f08bd562a4149e25f1
Mark Hammond <mhammond@skippinet.com.au>
2003-10-31 06:58:48 +0000

    Demonstrate/test PyIShellFolder's __iter__.

--- com/win32comext/shell/test/testShellFolder.py
+++ com/win32comext/shell/test/testShellFolder.py
@@ -2,10 +2,20 @@ from win32com.shell import shell
 from win32com.shell.shellcon import *
 
 sf = shell.SHGetDesktopFolder()
-print "sf is", sf
-enum = sf.EnumObjects(0, SHCONTF_FOLDERS | SHCONTF_NONFOLDERS | SHCONTF_INCLUDEHIDDEN)
-print "enum is", enum
-for i in enum:
-    name = sf.GetDisplayNameOf(i, SHGDN_NORMAL)
-    print name
+print "Shell Folder is", sf
 
+names = []
+for i in sf: # Magically calls EnumObjects
+    name = sf.GetDisplayNameOf(i, SHGDN_NORMAL)
+    names.append(name)
+
+# And get the enumerator manually    
+enum = sf.EnumObjects(0, SHCONTF_FOLDERS | SHCONTF_NONFOLDERS | SHCONTF_INCLUDEHIDDEN)
+num = 0
+for i in enum:
+    num += 1
+if num != len(names):
+    print "Should have got the same number of names!?"
+print "Found", len(names), "items on the desktop"
+for name in names:
+    print name
