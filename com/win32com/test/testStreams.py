f322933a4d7dcfc93076a3e01bdd5c2071f78899
Mark Hammond <mhammond@skippinet.com.au>
2003-11-02 10:04:07 +0000

    Convert to unittest tests

--- com/win32com/test/testStreams.py
+++ com/win32com/test/testStreams.py
@@ -2,26 +2,27 @@ import pythoncom
 import win32com.server.util
 import util
 
+import unittest
+
 class Persists:
     _public_methods_ = [ 'GetClassID', 'IsDirty', 'Load', 'Save',
                          'GetSizeMax', 'InitNew' ]
     _com_interfaces_ = [ pythoncom.IID_IPersistStreamInit ]
-
+    def __init__(self):
+        self.data = "abcdefg"
+        self.dirty = 1
     def GetClassID(self):
         return pythoncom.IID_NULL
-
     def IsDirty(self):
-        return 1
-
+        return self.dirty
     def Load(self, stream):
-        print "loaded:", stream.Read(26)
-
+        self.data = stream.Read(26)
     def Save(self, stream, clearDirty):
-        stream.Write('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        print "(saved state)"
-
+        stream.Write(self.data)
+        if clearDirty:
+            self.dirty = 0
     def GetSizeMax(self):
-        return 26
+        return 1024
 
     def InitNew(self):
         pass
@@ -46,36 +47,41 @@ class Stream:
         return len(data)
 
 
-def test():
-    mydata = 'abcdefghijklmnopqrstuvwxyz'
+class StreamTest(unittest.TestCase):
+    def _readWrite(self, data, write_stream, read_stream = None):
+        if read_stream is None: read_stream = write_stream
+        write_stream.Write(data)
+        got = read_stream.Read(len(data))
+        self.assertEqual(data, got)
 
-    # First test the objects just as Python objects...
-    s = Stream(mydata)
-    p = Persists()
+    def testit(self):
+        mydata = 'abcdefghijklmnopqrstuvwxyz'
+    
+        # First test the objects just as Python objects...
+        s = Stream(mydata)
+        p = Persists()
+    
+        p.Load(s)
+        p.Save(s, 0)
+        self.assertEqual(s.data, mydata)
+    
+        # Wrap the Python objects as COM objects, and make the calls as if
+        # they were non-Python COM objects.
+        s2 = win32com.server.util.wrap(s, pythoncom.IID_IStream)
+        p2 = win32com.server.util.wrap(p, pythoncom.IID_IPersistStreamInit)
 
-    p.Load(s)
-    p.Save(s, 0)
-    print "new state:", s.data
+        self._readWrite(mydata, s, s)
+        self._readWrite(mydata, s, s2)
+        self._readWrite(mydata, s2, s)
+        self._readWrite(mydata, s2, s2)
 
-    # reset the stream
-    s.Write(mydata)
-
-    # Wrap the Python objects as COM objects, and make the calls as if
-    # they were non-Python COM objects.
-    s2 = win32com.server.util.wrap(s, pythoncom.IID_IStream)
-    p2 = win32com.server.util.wrap(p, pythoncom.IID_IPersistStreamInit)
-
-    print "read:", s2.Read(26)
-    s2.Write("kilroy was here")
-    print "new state:", s.data
-
-    # reset the stream
-    s.Write(mydata)
-
-    p2.Load(s2)
-    p2.Save(s2, 0)
-    print "new state:", s.data
+        self._readWrite("string with\0a NULL", s2, s2)
+        # reset the stream
+        s.Write(mydata)
+        p2.Load(s2)
+        p2.Save(s2, 0)
+        self.assertEqual(s.data, mydata)
 
 if __name__=='__main__':
-    test()
+    unittest.main()
     util.CheckClean()
--- com/win32com/test/testWMI.py
+++ com/win32com/test/testWMI.py
@@ -1,9 +1,16 @@
 from win32com.client import GetObject
 
-def test():
-    cses = GetObject("WinMgMts:").InstancesOf("Win32_Process")
-    for cs in cses:
-        print cs.Properties_("Caption").Value
+import unittest
+
+class Simple(unittest.TestCase):
+    def testit(self):
+        cses = GetObject("WinMgMts:").InstancesOf("Win32_Process")
+        vals = []
+        for cs in cses:
+            val = cs.Properties_("Caption").Value
+            vals.append(val)
+        self.failIf(len(vals)<5, "We only found %d processes!" % len(vals))
 
 if __name__=='__main__':
-    test()
+    unittest.main()
+
