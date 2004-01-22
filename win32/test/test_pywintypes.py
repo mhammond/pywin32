4cbc1a80cf406a4e0c65b86a054991e02c8b52be
Mark Hammond <mhammond@skippinet.com.au>
2004-01-22 04:39:48 +0000

    Add a test that would crash PyTime.Format()

--- win32/test/test_pywintypes.py
+++ win32/test/test_pywintypes.py
@@ -10,6 +10,17 @@ class TestCase(unittest.TestCase):
         format_string = "%a %A %b %B %c %d %H %I %j %m %M %p %S %U %w %W %x %X %y %Y %Z"
         self.assertEquals(pytime_current.Format(format_string), time.strftime(format_string, struct_current))
 
+    def testPyTimePrint(self):
+        # This used to crash with an invalid, or too early time.
+        # We don't really want to check that it does cause a ValueError
+        # (as hopefully this wont be true forever).  So either working, or 
+        # ValueError is OK.
+        t = pywintypes.Time(-2)
+        try:
+            t.Format()
+        except ValueError:
+            return
+
 if __name__ == '__main__':
     unittest.main()
 
