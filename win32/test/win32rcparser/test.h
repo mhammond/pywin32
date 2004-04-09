4fe162b8a777911c285a3a2352a3c35de8481a6b
Mark Hammond <mhammond@skippinet.com.au>
2004-04-09 11:13:20 +0000

    New win32rcparser module for parsing Windows .rc files, and demos and tests

---
+++ win32/test/win32rcparser/test.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by test.rc
+//
+#define IDS_TEST_STRING1                1
+#define IDS_TEST_STRING2                2
+#define IDS_TEST_STRING3                3
+#define IDS_TEST_STRING4                4
+#define IDS_TEST_STRING5                5
+#define IDS_TEST_STRING6                6
+#define IDS_TEST_STRING7                7
+#define IDD_TEST_DIALOG1                101
+#define IDD_TEST_DIALOG2                102
+#define IDI_PYTHON                      102
+#define IDB_PYTHON                      103
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
