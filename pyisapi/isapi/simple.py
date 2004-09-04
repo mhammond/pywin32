78b816fca702f221c04d8ba77731c8398907409d
Mark Hammond <mhammond@skippinet.com.au>
2004-09-04 09:36:12 +0000

    * Add base-class constructors, incase we add something later, and Init functions handle reload

--- pyisapi/isapi/simple.py
+++ pyisapi/isapi/simple.py
@@ -2,8 +2,13 @@
 
 class SimpleExtension:
     "Base class for a a simple ISAPI extension"
+    def __init__(self):
+        pass
+
     def GetExtensionVersion(self, vi):
-        vi.ExtensionDesc = self.__doc__
+        # nod to our reload capability - vi is None when we are reloaded.
+        if vi is not None:
+            vi.ExtensionDesc = self.__doc__
 
     def HttpExtensionProc(self, control_block):
         raise NotImplementedError, "sub-classes should override HttpExtensionProc"
@@ -14,11 +19,16 @@ class SimpleExtension:
 class SimpleFilter:
     "Base class for a a simple ISAPI filter"
     filter_flags = None
+    def __init__(self):
+        pass
+
     def GetFilterVersion(self, fv):
         if self.filter_flags is None:
             raise RuntimeError, "You must specify the filter flags"
-        fv.Flags = self.filter_flags
-        fv.FilterDesc = self.__doc__
+        # nod to our reload capability - fv is None when we are reloaded.
+        if fv is not None:
+            fv.Flags = self.filter_flags
+            fv.FilterDesc = self.__doc__
 
     def HttpFilterProc(self, fc):
         raise NotImplementedError, "sub-classes should override HttpExtensionProc"
