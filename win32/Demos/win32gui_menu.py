67e04093689cb9a5bca130636ccd918288ffc3f3
Mark Hammond <mhammond@skippinet.com.au>
2004-09-07 02:05:36 +0000

    Fix the demo to correctly work with checkbox items, radio-button items, and demonstrate using an icon in a menu.

--- win32/Demos/win32gui_menu.py
+++ win32/Demos/win32gui_menu.py
@@ -9,7 +9,11 @@
 
 # Based on Andy McKay's demo code.
 from win32api import *
-from win32gui import *
+# Try and use XP features, so we get alpha-blending etc.
+try:
+    from winxpgui import *
+except ImportError:
+    from win32gui import *
 from win32gui_struct import *
 import win32con
 import sys, os
@@ -45,14 +49,106 @@ class MainWindow:
             icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
             hicon = LoadImage(hinst, iconPathName, win32con.IMAGE_ICON, 0, 0, icon_flags)
         else:
+            iconPathName = None
             print "Can't find a Python icon file - using default"
             hicon = LoadIcon(0, win32con.IDI_APPLICATION)
+        self.iconPathName = iconPathName
         
+        self.createMenu()
+
         flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
         nid = (self.hwnd, 0, flags, win32con.WM_USER+20, hicon, "Python Demo")
         Shell_NotifyIcon(NIM_ADD, nid)
         print "Please right-click on the Python icon in the taskbar"
 
+    def createMenu(self):
+        self.hmenu = menu = CreatePopupMenu()
+        # Create our 'Exit' item with the standard, ugly 'close' icon.
+        item, extras = PackMENUITEMINFO(text = "Exit",
+                                        hbmpItem=win32con.HBMMENU_MBAR_CLOSE,
+                                        wID=1000)
+        InsertMenuItem(menu, 0, 1, item)
+        # Create a 'text only' menu via InsertMenuItem rather then
+        # AppendMenu, just to prove we can!
+        item, extras = PackMENUITEMINFO(text = "Text only item",
+                                        wID=1001)
+        InsertMenuItem(menu, 0, 1, item)
+
+        load_bmp_flags=win32con.LR_LOADFROMFILE | \
+                       win32con.LR_LOADTRANSPARENT
+        # These images are "over sized", so we load them scaled.
+        hbmp = LoadImage(0, os.path.join(this_dir, "images/smiley.bmp"),
+                         win32con.IMAGE_BITMAP, 20, 20, load_bmp_flags)
+
+        # Create a top-level menu with a bitmap
+        item, extras = PackMENUITEMINFO(text="Menu with bitmap",
+                                        hbmpItem=hbmp,
+                                        wID=1002)
+        InsertMenuItem(menu, 0, 1, item)
+        
+        # Create one with an icon - this is a fair bit more work, as we need
+        # to convert the icon to a bitmap.
+        # First load the icon.
+        ico_x = GetSystemMetrics(win32con.SM_CXSMICON)
+        ico_y = GetSystemMetrics(win32con.SM_CYSMICON)
+        if self.iconPathName:
+            hicon = LoadImage(0, self.iconPathName, win32con.IMAGE_ICON, ico_x, ico_y, win32con.LR_LOADFROMFILE)
+        else:
+            shell_dll = os.path.join(GetSystemDirectory(), "shell32.dll")
+            large, small = win32gui.ExtractIconEx(shell_dll, 4, 1)
+            hicon = small[0]
+            DestroyIcon(large[0])
+
+        hdcBitmap = CreateCompatibleDC(0)
+        hdcScreen = GetDC(0)
+        hbm = CreateCompatibleBitmap(hdcScreen, ico_x, ico_y)
+        hbmOld = SelectObject(hdcBitmap, hbm)
+        # Fill the background.
+        brush = GetSysColorBrush(win32con.COLOR_MENU)
+        FillRect(hdcBitmap, (0, 0, 16, 16), brush)
+        # unclear if brush needs to be feed.  Best clue I can find is:
+        # "GetSysColorBrush returns a cached brush instead of allocating a new
+        # one." - implies no DeleteObject
+        # draw the icon
+        DrawIconEx(hdcBitmap, 0, 0, hicon, ico_x, ico_y, 0, 0, win32con.DI_NORMAL)
+        SelectObject(hdcBitmap, hbmOld)
+        DeleteDC(hdcBitmap)
+
+        item, extras = PackMENUITEMINFO(text="Menu with icon",
+                                        hbmpItem=hbm,
+                                        wID=1009)
+        InsertMenuItem(menu, 0, 1, item)
+
+        # Create a sub-menu, and put a few funky ones there.
+        self.sub_menu = sub_menu = CreatePopupMenu()
+        # A 'checkbox' menu.
+        item, extras = PackMENUITEMINFO(fState=win32con.MFS_CHECKED,
+                                        text="Checkbox menu",
+                                        hbmpItem=hbmp,
+                                        wID=1003)
+        InsertMenuItem(sub_menu, 0, 1, item)
+        # A 'radio' menu.
+        InsertMenu(sub_menu, 0, win32con.MF_BYPOSITION, win32con.MF_SEPARATOR, None)
+        item, extras = PackMENUITEMINFO(fType=win32con.MFT_RADIOCHECK,
+                                        fState=win32con.MFS_CHECKED,
+                                        text="Checkbox menu - bullet 1",
+                                        hbmpItem=hbmp,
+                                        wID=1004)
+        InsertMenuItem(sub_menu, 0, 1, item)
+        item, extras = PackMENUITEMINFO(fType=win32con.MFT_RADIOCHECK,
+                                        fState=win32con.MFS_UNCHECKED,
+                                        text="Checkbox menu - bullet 2",
+                                        hbmpItem=hbmp,
+                                        wID=1005)
+        InsertMenuItem(sub_menu, 0, 1, item)
+        # And add the sub-menu to the top-level menu.
+        item, extras = PackMENUITEMINFO(text="Sub-Menu",
+                                        hSubMenu=sub_menu)
+        InsertMenuItem(menu, 0, 1, item)
+
+        # Set 'Exit' as the default option.
+        SetMenuDefaultItem(menu, 1000, 0)
+        
     def OnDestroy(self, hwnd, msg, wparam, lparam):
         nid = (self.hwnd, 0)
         Shell_NotifyIcon(NIM_DELETE, nid)
@@ -61,66 +157,19 @@ class MainWindow:
     def OnTaskbarNotify(self, hwnd, msg, wparam, lparam):
         if lparam==win32con.WM_RBUTTONUP:
             print "You right clicked me."
-            menu = CreatePopupMenu()
-            # Create our 'Exit' item with the standard, ugly 'close' icon.
-            item, extras = PackMENUITEMINFO(text = "Exit",
-                                            hbmpItem=win32con.HBMMENU_MBAR_CLOSE,
-                                            wID=1000)
-            InsertMenuItem(menu, 0, 1, item)
-            # Create a 'text only' menu via InsertMenuItem rather then
-            # AppendMenu, just to prove we can!
-            item, extras = PackMENUITEMINFO(text = "Text only item",
-                                            wID=1001)
-            InsertMenuItem(menu, 0, 1, item)
-
-            load_bmp_flags=win32con.LR_LOADFROMFILE | \
-                           win32con.LR_LOADTRANSPARENT
-            # These images are "over sized", so we load them scaled.
-            hbmp = LoadImage(0, os.path.join(this_dir, "images/smiley.bmp"),
-                             win32con.IMAGE_BITMAP, 20, 20, load_bmp_flags)
-
-            # Create a top-level menu with a bitmap
-            item, extras = PackMENUITEMINFO(text="Menu with icon",
-                                            hbmpItem=hbmp,
-                                            wID=1002)
-            InsertMenuItem(menu, 0, 1, item)
-
-            # Create a sub-menu, and put a few funky ones there.
-            self.sub_menu = sub_menu = CreatePopupMenu()
-            # A 'checkbox' menu.
-            item, extras = PackMENUITEMINFO(fState=win32con.MFS_CHECKED,
-                                            text="Checkbox menu",
-                                            hbmpItem=hbmp,
-                                            wID=1003)
-            InsertMenuItem(sub_menu, 0, 1, item)
-            # A 'radio' menu.
-            InsertMenu(sub_menu, 0, win32con.MF_BYPOSITION, win32con.MF_SEPARATOR, None)
-            item, extras = PackMENUITEMINFO(fType=win32con.MFT_RADIOCHECK,
-                                            fState=win32con.MFS_CHECKED,
-                                            text="Checkbox menu - bullet 1",
-                                            hbmpItem=hbmp,
-                                            wID=1004)
-            InsertMenuItem(sub_menu, 0, 1, item)
-            item, extras = PackMENUITEMINFO(fType=win32con.MFT_RADIOCHECK,
-                                            fState=win32con.MFS_UNCHECKED,
-                                            text="Checkbox menu - bullet 2",
-                                            hbmpItem=hbmp,
-                                            wID=1005)
-            InsertMenuItem(sub_menu, 0, 1, item)
-            # And add the sub-menu to the top-level menu.
-            item, extras = PackMENUITEMINFO(text="Sub-Menu",
-                                            hSubMenu=sub_menu)
-            InsertMenuItem(menu, 0, 1, item)
-
-            # Set 'Exit' as the default option.
-            SetMenuDefaultItem(menu, 1000, 0)
-
-            # And display the menu at the cursor pos.
+            # display the menu at the cursor pos.
             pos = GetCursorPos()
             SetForegroundWindow(self.hwnd)
-            TrackPopupMenu(menu, win32con.TPM_LEFTALIGN, pos[0], pos[1], 0, self.hwnd, None)
+            TrackPopupMenu(self.hmenu, win32con.TPM_LEFTALIGN, pos[0], pos[1], 0, self.hwnd, None)
             PostMessage(self.hwnd, win32con.WM_NULL, 0, 0)
-            self.hmenu = menu
+        elif lparam==win32con.WM_LBUTTONDBLCLK:
+            print "You double-clicked me"
+            # find the default menu item and fire it.
+            cmd = GetMenuDefaultItem(self.hmenu, False, 0)
+            if cmd == -1:
+                print "Can't find a default!"
+            # and just pretend it came from the menu
+            self.OnCommand(hwnd, win32con.WM_COMMAND, cmd, 0)
         return 1
 
     def OnCommand(self, hwnd, msg, wparam, lparam):
@@ -131,30 +180,29 @@ class MainWindow:
         if id == 1000:
             print "Goodbye"
             DestroyWindow(self.hwnd)
-        elif id==1003:
-            # Our 'checkbox' item
+        elif id in (1003, 1004, 1005):
+            # Our 'checkbox' and 'radio' items
             state = GetMenuState(self.sub_menu, id, win32con.MF_BYCOMMAND)
             if state==-1:
                 raise RuntimeError, "No item found"
             if state & win32con.MF_CHECKED:
                 check_flags = win32con.MF_UNCHECKED
                 print "Menu was checked - unchecking"
-            elif state & win32con.MF_UNCHECKED:
+            else:
                 check_flags = win32con.MF_CHECKED
                 print "Menu was unchecked - checking"
-            else:
-                raise RuntimeError, "Menu is neither checked nor unchecked!"
 
-            CheckMenuItem(self.sub_menu, id, win32con.MF_BYCOMMAND & check_flags)
-            if GetMenuState(self.sub_menu, id, win32con.MF_BYCOMMAND) & win32con.MF_CHECKED != check_flags:
-                raise RuntimeError, "The new item didn't get the new checked state!"
-        elif id==1004 or id==1005:
-            # Our 'checkbox' item
-            state = GetMenuState(self.sub_menu, id, win32con.MF_BYCOMMAND)
-            if state==-1:
-                raise RuntimeError, "No item found"
-            CheckMenuItem(self.sub_menu, id, win32con.MF_BYCOMMAND & win32con.MF_CHECKED)
-            if GetMenuState(self.sub_menu, id, win32con.MF_BYCOMMAND) & win32con.MF_CHECKED == 0:
+            if id == 1003:
+                # simple checkbox
+                rc = CheckMenuItem(self.sub_menu, id,
+                                   win32con.MF_BYCOMMAND | check_flags)
+            else:
+                # radio button - must pass the first and last IDs in the
+                # "group", and the ID in the group that is to be selected.
+                rc = CheckMenuRadioItem(self.sub_menu, 1004, 1005, id,
+                                        win32con.MF_BYCOMMAND)
+            new_state = GetMenuState(self.sub_menu, id, win32con.MF_BYCOMMAND)
+            if  new_state & win32con.MF_CHECKED != check_flags:
                 raise RuntimeError, "The new item didn't get the new checked state!"
         else:
             print "OnCommand for ID", id
