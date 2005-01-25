8abb07886618009a5fb00c3d7c42671021b3e24c
Mark Hammond <mhammond@skippinet.com.au>
2005-01-25 09:47:00 +0000

    Add BindIFilterFromStream

--- com/win32comext/ifilter/src/PyIFilter.cpp
+++ com/win32comext/ifilter/src/PyIFilter.cpp
@@ -93,9 +93,6 @@ PyObject *PyIFilter::GetChunk(PyObject *self, PyObject *args)
 									  stat.idChunkSource,
 									  stat.cwcStartSource,
 									  stat.cwcLenSource);
-
-						
-
 }
 
 // @pymethod |PyIFilter|GetText|Description of GetText.
@@ -230,6 +227,35 @@ static PyObject *pyBindIFilterFromStorage(PyObject *self, PyObject *args)
 	return ret;
 }
 
+static PyObject *pyBindIFilterFromStream(PyObject *self, PyObject *args)
+{
+	HRESULT hr;
+	IUnknown *pOb = NULL;
+	PyObject *obStg;
+	
+	PyObject *ret;
+	long lres = 0;
+	if (!PyArg_ParseTuple(args, "O:BindIFilterFromStream", &obStg)) 
+		return NULL;
+
+	IStream *pstm;
+	BOOL bPythonIsHappy = TRUE;
+	if (!PyCom_InterfaceFromPyObject(obStg, IID_IStream, (void **)&pstm, FALSE /* bNoneOK */))
+		bPythonIsHappy = FALSE;
+
+	if (!bPythonIsHappy)
+		return NULL;
+
+	Py_BEGIN_ALLOW_THREADS;
+	hr = BindIFilterFromStream( pstm , NULL , (void**)&pOb ); 
+	pstm->Release();
+	Py_END_ALLOW_THREADS;
+	if (FAILED(hr))
+		ret = OleSetOleError(hr);
+	else
+		ret = PyCom_PyObjectFromIUnknown(pOb, IID_IFilter, FALSE);
+	return ret;
+}
 
 static int AddConstant(PyObject *dict, const char *key, long value)
 {
@@ -274,7 +300,8 @@ PyComTypeObject PyIFilter::type("PyIFilter",
 static struct PyMethodDef g_methods[] =
 {
 	{ "LoadIFilter", pyLoadIFilter, 1 }, // @pymeth Init|Description of Init
-	{ "BindIFilterFromStorage", pyBindIFilterFromStorage, 1 }, // @pymeth Init|Description of Init
+	{ "BindIFilterFromStorage", pyBindIFilterFromStorage, 1 }, // @pymeth BindIFilterFromStorage|
+	{ "BindIFilterFromStream", pyBindIFilterFromStream, 1 }, // @pymeth BindIFilterFromStream|
 	{ NULL }
 };
 
