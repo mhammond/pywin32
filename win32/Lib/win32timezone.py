9c9603879657ae73da7d72bf922dc2e3db11dbba
Mark Hammond <mhammond@skippinet.com.au>
2005-04-12 06:14:01 +0000

    Move __future__ statement to the top of the file.

--- win32/Lib/win32timezone.py
+++ win32/Lib/win32timezone.py
@@ -63,13 +63,13 @@ registry.
 '(GMT) Greenwich Mean Time : Dublin, Edinburgh, Lisbon, London'
 
 """
+from __future__ import generators
 
 __author__ = 'Jason R. Coombs <jaraco@jaraco.com>'
 __version__ = '$Revision$'[11:-2]
 __vssauthor__ = '$Author$'[9:-2]
 __date__ = '$Modtime: 04-04-14 10:52 $'[10:-2]
 
-from __future__ import generators
 import os, win32api, win32con, struct, datetime
 
 class TimeZoneInfo( datetime.tzinfo ):
