42663e8a83d56709fec1e7b5ff0f321c440c7ef6
Roger Upole <rupole@spideroak.com>
2005-05-18 16:41:52 +0000

    Remove hardcoded print processor name and add an extra CreateCompatibleDC so demo works with more types of printers, use API functions instead of MFC object methods

--- win32/Demos/print_desktop.py
+++ win32/Demos/print_desktop.py
@@ -1,10 +1,10 @@
-import win32print, pywintypes, win32con, win32ui, win32gui
+import win32print, pywintypes, win32con, win32gui, win32ui
 
 pname=win32print.GetDefaultPrinter()
 print pname
 p=win32print.OpenPrinter(pname)
 print 'Printer handle: ',p
-
+print_processor=win32print.GetPrinter(p,2)['pPrintProcessor']
 ## call with last parm set to 0 to get total size needed for printer's DEVMODE
 dmsize=win32print.DocumentProperties(0, p, pname, None, None, 0)
 ## dmDriverExtra should be total size - fixed size
@@ -15,25 +15,37 @@ dm.Orientation=win32con.DMORIENT_LANDSCAPE
 dm.Copies=2
 win32print.DocumentProperties(0, p, pname, dm, dm, win32con.DM_IN_BUFFER|win32con.DM_OUT_BUFFER)
 
-pDC=win32gui.CreateDC('WINSPOOL',pname,dm)
-printerDC=win32ui.CreateDCFromHandle(pDC)
-
-printerwidth=printerDC.GetDeviceCaps(110)  ##PHYSICALWIDTH
-printerheight=printerDC.GetDeviceCaps(111) ##PHYSICALHEIGHT
+pDC=win32gui.CreateDC(print_processor,pname,dm)
+printerwidth=win32ui.GetDeviceCaps(pDC, 110) ##PHYSICALWIDTH
+printerheight=win32ui.GetDeviceCaps(pDC, 111) ##PHYSICALHEIGHT
 
 hwnd=win32gui.GetDesktopWindow()
-## hwnd=win32gui.GetForegroundWindow()
 l,t,r,b=win32gui.GetWindowRect(hwnd)
 desktopheight=b-t
 desktopwidth=r-l
 dDC = win32gui.GetWindowDC(hwnd)
-desktopDC=win32ui.CreateDCFromHandle(dDC)
 
-printerDC.StartDoc('desktop.bmp')
-printerDC.StartPage()
-printerDC.StretchBlt((0,0),(int(printerwidth*.9),int(printerheight*.9)),  ## allow for paper margin
-           desktopDC,(0,0),(desktopwidth,desktopheight),win32con.SRCCOPY)
-printerDC.EndPage()
-printerDC.EndDoc()
+dcDC=win32gui.CreateCompatibleDC(dDC)
+dcBM = win32gui.CreateCompatibleBitmap(dDC, desktopwidth, desktopheight);
+win32gui.SelectObject(dcDC, dcBM)
+win32gui.StretchBlt(dcDC, 0, 0, desktopwidth, desktopheight, dDC, 0, 0, desktopwidth, desktopheight, win32con.SRCCOPY)
+
+pcDC=win32gui.CreateCompatibleDC(pDC)
+pcBM=win32gui.CreateCompatibleBitmap(pDC, printerwidth, printerheight)
+win32gui.SelectObject(pcDC, pcBM)
+win32gui.StretchBlt(pcDC, 0, 0, printerwidth, printerheight, dcDC, 0, 0, desktopwidth, desktopheight, win32con.SRCCOPY)
+
+win32print.StartDoc(pDC,('desktop.bmp',None,None,0))
+win32print.StartPage(pDC)
+win32gui.StretchBlt(pDC, 0, 0, int(printerwidth*.9), int(printerheight*.9), pcDC, 0, 0, printerwidth, printerheight, win32con.SRCCOPY)
+
+win32print.EndPage(pDC)
+win32print.EndDoc(pDC)
+
+win32print.ClosePrinter(p)
+win32gui.DeleteDC(dDC)
+win32gui.DeleteDC(dcDC)
+win32gui.DeleteDC(pDC)
+win32gui.DeleteDC(pcDC)
 
 
