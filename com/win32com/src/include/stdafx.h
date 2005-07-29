78e9fd2098fef76618894d19b86eaf5ced94fb71
Mark Hammond <mhammond@skippinet.com.au>
2005-07-29 05:11:19 +0000

    Get things working with latest platform sdk.

--- com/win32com/src/include/stdafx.h
+++ com/win32com/src/include/stdafx.h
@@ -7,6 +7,7 @@
 // so we dont lose all the constants etc that come with DCOM
 //
 #define _WIN32_DCOM
-
+#define _WIN32_WINNT 0x0501 // we use some of these features.
 // PyWinTypes.h pulls in Python.h and windows.h.
+
 #include <PyWinTypes.h>
