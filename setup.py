afd1678c610c95b9cfccf67a22607796dd441ba5
Roger Upole <rupole@spideroak.com>
2006-05-26 03:22:31 +0000

    Add source file to win32api, specify sources so .dsp file can be removed

--- setup.py
+++ setup.py
@@ -1009,7 +1009,7 @@ for info in (
         ("perfmon", "", True),
         ("timer", "user32", False),
         ("win2kras", "rasapi32", False, 0x0500),
-        ("win32api", "user32 advapi32 shell32 version", False, 0x0500),
+        ("win32api", "user32 advapi32 shell32 version", False, 0x0500, 'win32/src/win32apimodule.cpp win32/src/win32api_display.cpp'),
         ("win32crypt", "Crypt32", False, 0x0500, 'win32/src/win32crypt.i win32/src/win32cryptmodule.cpp'),
         ("win32file", "oleaut32", False, 0x0500),
         ("win32event", "user32", False),
