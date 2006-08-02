517764a924ba6d61dd74b4a8a32c8546f7c55847
Mark Hammond <mhammond@skippinet.com.au>
2006-08-02 23:04:31 +0000

    unofficial 209.1 builds

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-build_id="209" # may optionally include a ".{patchno}" suffix.
+build_id="209.1" # may optionally include a ".{patchno}" suffix.
 # Putting buildno at the top prevents automatic __doc__ assignment, and
 # I *want* the build number at the top :)
 __doc__="""This is a distutils setup-script for the pywin32 extensions
