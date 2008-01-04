c3af10041d4fb95b79e5e583777f23459821426f
cvs2hg <devnull@localhost>
2008-01-04 18:49:11 +0000

    fixup commit for tag 'adodbapi_2_1'

--- com/win32comext/axscript/src/stdafx.cpp
+++
@@ -1,3 +0,0 @@
-// stdafx.cpp
-
-#include "stdafx.h"
--- setup_win32all.py
+++
@@ -1,6 +0,0 @@
-# The build script used to be called setup_win32all.py
-# We will remove this in a few versions, but give people the hint
-print "Please use 'setup.py' instead of 'setup_win32all.py'"
-import sys, os
-mydir=os.path.dirname(sys.argv[0])
-execfile(os.path.join(mydir, "setup.py"))
