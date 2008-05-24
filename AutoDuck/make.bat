0f4145f6aba622275b7990565591d9de79a64a33
Mark Hammond <mhammond@skippinet.com.au>
2008-05-24 08:18:27 +0000

    Make it usually magically work on amd64 archs.

--- AutoDuck/make.bat
+++ AutoDuck/make.bat
@@ -5,6 +5,8 @@ if not errorlevel 1 set HHC=hhc & goto doit
 
 if exist "\Program files\HTML Help Workshop\hhc.exe" set HHC="\Program files\HTML Help Workshop\hhc.exe" & goto doit
 if exist "C:\Program files\HTML Help Workshop\hhc.exe" set HHC="C:\Program files\HTML Help Workshop\hhc.exe" & goto doit
+if exist "\Program files (x86)\HTML Help Workshop\hhc.exe" set HHC="\Program files (x86)\HTML Help Workshop\hhc.exe" & goto doit
+if exist "C:\Program files (x86)\HTML Help Workshop\hhc.exe" set HHC="C:\Program files (x86)\HTML Help Workshop\hhc.exe" & goto doit
 
 echo Can not locate HHC.EXE - please set the HHC environment to point to the .exe
 goto xit
