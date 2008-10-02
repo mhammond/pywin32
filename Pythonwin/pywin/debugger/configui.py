1f80560165dbc2ebf29930ca04815de484c7c181
Mark Hammond <mhammond@skippinet.com.au>
2008-10-02 11:16:53 +0000

    merge from py3k branch - remove 'import *'

--- Pythonwin/pywin/debugger/configui.py
+++ Pythonwin/pywin/debugger/configui.py
@@ -1,16 +1,17 @@
-from dbgcon import *
+import dbgcon
 from pywin.mfc import dialog
+import win32ui
 
 class DebuggerOptionsPropPage(dialog.PropertyPage):
 	def __init__(self):
 		dialog.PropertyPage.__init__(self, win32ui.IDD_PP_DEBUGGER)
 
 	def OnInitDialog(self):
-		options = self.options = LoadDebuggerOptions()
-		self.AddDDX(win32ui.IDC_CHECK1, OPT_HIDE)
-		self[OPT_STOP_EXCEPTIONS] = options[OPT_STOP_EXCEPTIONS]
-		self.AddDDX(win32ui.IDC_CHECK2, OPT_STOP_EXCEPTIONS)
-		self[OPT_HIDE] = options[OPT_HIDE]
+		options = self.options = dbgcon.LoadDebuggerOptions()
+		self.AddDDX(win32ui.IDC_CHECK1, dbgcon.OPT_HIDE)
+		self[dbgcon.OPT_STOP_EXCEPTIONS] = options[dbgcon.OPT_STOP_EXCEPTIONS]
+		self.AddDDX(win32ui.IDC_CHECK2, dbgcon.OPT_STOP_EXCEPTIONS)
+		self[dbgcon.OPT_HIDE] = options[dbgcon.OPT_HIDE]
 		return dialog.PropertyPage.OnInitDialog(self)
 
 	def OnOK(self):
@@ -22,7 +23,7 @@ class DebuggerOptionsPropPage(dialog.PropertyPage):
 					self.options[key] = val
 					dirty = 1
 		if dirty:
-			SaveDebuggerOptions(self.options)
+			dbgcon.SaveDebuggerOptions(self.options)
 		# If there is a debugger open, then set its options.
 		import pywin.debugger
 		if pywin.debugger.currentDebugger is not None:
