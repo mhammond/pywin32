e7a28984fe7005ddad780628d7fbfca4c55b00b5
Mark Hammond <mhammond@skippinet.com.au>
2008-12-11 06:58:21 +0000

    Use .encode('ascii') to make py3k friendly

--- win32/Demos/win32fileDemo.py
+++ win32/Demos/win32fileDemo.py
@@ -21,13 +21,13 @@ def SimpleFileDemo():
                                   0, 
                                   None)
     test_data = "Hello\0there"
-    win32file.WriteFile(handle, test_data)
+    win32file.WriteFile(handle, test_data.encode('ascii'))
     handle.Close()
     # Open it for reading.
     handle = win32file.CreateFile(testName, win32file.GENERIC_READ, 0, None, win32con.OPEN_EXISTING, 0, None)
     rc, data = win32file.ReadFile(handle, 1024)
     handle.Close()
-    if data == test_data:
+    if data.decode('ascii') == test_data:
         print "Successfully wrote and read a file"
     os.unlink(testName)
 
