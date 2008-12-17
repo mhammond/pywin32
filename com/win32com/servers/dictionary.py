a345c510810968184735da4da1c25f7e1c386b2e
Mark Hammond <mhammond@skippinet.com.au>
2008-12-17 12:57:44 +0000

    fix silly error I introduced in py3k merging

--- com/win32com/servers/dictionary.py
+++ com/win32com/servers/dictionary.py
@@ -73,7 +73,7 @@ class DictionaryPolicy(policy.BasicWrapPolicy):
         raise COMException(desc="not enough parameters", scode=winerror.DISP_E_BADPARAMCOUNT)
 
       key = args[0]
-      if type(exp) not in [str, unicode]:
+      if type(key) not in [str, unicode]:
         ### the nArgErr thing should be 0-based, not reversed... sigh
         raise COMException(desc="Key must be a string", scode=winerror.DISP_E_TYPEMISMATCH)
 
