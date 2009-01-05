11c132c9bb6520ffc6683ee42726d327c1339861
Mark Hammond <mhammond@skippinet.com.au>
2009-01-05 11:03:05 +0000

    whitespace normalization

--- Pythonwin/pywin/framework/toolmenu.py
+++ Pythonwin/pywin/framework/toolmenu.py
@@ -13,11 +13,11 @@ idPos = 100
 # The default items should no tools menu exist in the INI file.
 defaultToolMenuItems = [
 	('Browser', 'win32ui.GetApp().OnViewBrowse(0,0)'),
-       ('Browse PythonPath', 'from pywin.tools import browseProjects;browseProjects.Browse()'),
-       ('Edit Python Path', 'from pywin.tools import regedit;regedit.EditRegistry()'),
-       ('COM Makepy utility', 'from win32com.client import makepy;makepy.main()'),
-       ('COM Browser', 'from win32com.client import combrowse;combrowse.main()'),
-       ('Trace Collector Debugging tool', 'from pywin.tools import TraceCollector;TraceCollector.MakeOutputWindow()'),
+	('Browse PythonPath', 'from pywin.tools import browseProjects;browseProjects.Browse()'),
+	('Edit Python Path', 'from pywin.tools import regedit;regedit.EditRegistry()'),
+	('COM Makepy utility', 'from win32com.client import makepy;makepy.main()'),
+	('COM Browser', 'from win32com.client import combrowse;combrowse.main()'),
+	('Trace Collector Debugging tool', 'from pywin.tools import TraceCollector;TraceCollector.MakeOutputWindow()'),
 ]
 
 def LoadToolMenuItems():
