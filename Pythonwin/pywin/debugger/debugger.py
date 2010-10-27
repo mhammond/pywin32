076a4456507457f6478114d446f6b72b50b0d26d
Mark Hammond <mhammond@skippinet.com.au>
2010-10-27 01:25:28 +0000

    fix crash removing breakpoint from breakpoint window (bug 3058010)

--- Pythonwin/pywin/debugger/debugger.py
+++ Pythonwin/pywin/debugger/debugger.py
@@ -330,7 +330,7 @@ class DebuggerBreakpointsWindow(DebuggerListViewWindow):
 			num = self.GetNextItem(-1, commctrl.LVNI_SELECTED)
 			item_id = self.GetItem(num)[6]
 			from bdb import Breakpoint
-			for bplist in Breakpoint.bplist.itervalues():
+			for bplist in Breakpoint.bplist.values():
 				for bp in bplist:
 					if id(bp)==item_id:
 						self.debugger.clear_break(bp.file, bp.line)
