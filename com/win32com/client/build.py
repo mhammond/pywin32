52efc9c6ee4d686468b58531dae07edaccabbcd0
Mark Hammond <mhammond@skippinet.com.au>
2011-04-24 13:45:11 +1000

    3rd time lucky - do the right thing on py3k

--- com/win32com/client/build.py
+++ com/win32com/client/build.py
@@ -28,8 +28,10 @@ import datetime
 # It isn't really clear what the quoting rules are in a C/IDL string and
 # literals like a quote char and backslashes makes life a little painful to
 # always render the string perfectly - so just punt and fall-back to a repr()
-def _makeDocString(s, encoding="mbcs"):
-	return repr(s.encode(encoding))
+def _makeDocString(s):
+	if sys.version_info < (3,):
+		s = s.encode("mbcs")
+	return repr(s)
 
 error = "PythonCOM.Client.Build error"
 class NotSupportedException(Exception): pass # Raised when we cant support a param type.
