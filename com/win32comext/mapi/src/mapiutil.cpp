8cace02d040abd46f7f2777c620d20450d16cdf3
Mark Hammond <mhammond@skippinet.com.au>
2018-09-28 13:42:02 +1000

    Ensure Python error state is correct if a mapi value is the wrong type. Fixes #1250

--- com/win32comext/mapi/src/mapiutil.cpp
+++ com/win32comext/mapi/src/mapiutil.cpp
@@ -70,8 +70,10 @@ PyObject *PyObject_FromMAPIERROR(MAPIERROR *e, BOOL bIsUnicode, BOOL free_buffer
 
 BOOL AllocMVBuffer(PyObject *seq, size_t itemSize, void *pAllocMoreLinkBlock, void **pbuf, ULONG *pLen)
 {
-	if (!PySequence_Check(seq))
+	if (!PySequence_Check(seq)) {
+		PyErr_SetString(PyExc_TypeError, "A multi-valued SPropValue item must be a sequence");
 		return FALSE;
+	}
 	*pLen = PySequence_Length(seq);
 	int bufSize = *pLen * itemSize;
 	HRESULT hr = MAPIAllocateMore(bufSize, pAllocMoreLinkBlock, (void **)pbuf);
