6421597f04d7b675a7057644b5fe027ed472f3cb
Mark Hammond <mhammond@skippinet.com.au>
2020-04-26 16:44:47 +1000

    Add Py 3.8 and 3.9 to the setuptools classifications

--- setup.py
+++ setup.py
@@ -2475,6 +2475,8 @@ classifiers = [ 'Environment :: Win32 (MS Windows)',
 	            'Programming Language :: Python :: 3.5',
 	            'Programming Language :: Python :: 3.6',
 	            'Programming Language :: Python :: 3.7',
+	            'Programming Language :: Python :: 3.8',
+	            'Programming Language :: Python :: 3.9',
 	            'Programming Language :: Python :: Implementation :: CPython',
 	          ]
 
