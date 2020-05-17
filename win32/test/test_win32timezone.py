ad90c918a4f26b930b89c114eabb3272659b4d5c
Jason R. Coombs <jaraco@jaraco.com>
2020-05-16 20:23:02 -0400

    👹 Feed the hobgoblins (delint). (#1521)

--- win32/test/test_win32timezone.py
+++ win32/test/test_win32timezone.py
@@ -1,11 +1,15 @@
 # Test module for win32timezone
 
-import unittest, win32timezone, doctest
+import unittest
+import win32timezone
+import doctest
+
 
 class Win32TimeZoneTest(unittest.TestCase):
     def testWin32TZ(self):
-        failed, total = doctest.testmod( win32timezone, verbose = False )
-        self.failIf( failed )
+        failed, total = doctest.testmod(win32timezone, verbose=False)
+        self.failIf(failed)
+
 
 if __name__ == '__main__':
     unittest.main()
