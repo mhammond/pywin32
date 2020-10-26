ee847e5a3724369c197ce5a5ef775e5ccb61edb9
Peter Law <PeterJCLaw@gmail.com>
2020-10-26 20:07:42 +0000

    Use the README as the package's long description
    This should get the README showing on PyPI, as long as builds use
    a recent version of setuptools when run.

--- setup.py
+++ setup.py
@@ -2292,13 +2292,15 @@ classifiers = [ 'Environment :: Win32 (MS Windows)',
 	            'Programming Language :: Python :: Implementation :: CPython',
 	          ]
 
+my_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(my_dir, 'README.md')) as f:
+    long_description = f.read()
+
 dist = setup(name="pywin32",
       version=str(build_id),
       description="Python for Window Extensions",
-      long_description="Python extensions for Microsoft Windows\n"
-                       "Provides access to much of the Win32 API, the\n"
-                       "ability to create and use COM objects, and the\n"
-                       "Pythonwin environment.",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author="Mark Hammond (et al)",
       author_email = "mhammond@skippinet.com.au",
       url="https://github.com/mhammond/pywin32",
