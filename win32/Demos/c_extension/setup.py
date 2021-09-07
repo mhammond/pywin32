mhammond/pywin32

commit aa30f7586706fe6e562825dd56e50a95e57050aa

replace use of `distutils.sysconfig` with `sysconfig`

win32/Demos/c_extension/setup.py
@@ -4,16 +4,17 @@
 # Use 'python setup.py build' to build this extension.
 import os
 from distutils.core import setup, Extension
-from distutils.sysconfig import get_python_lib
+from sysconfig import get_paths
 
 sources = ["win32_extension.cpp"]
+libdir = sysconfig.get_paths()["platlib"]
 
 # Specify the directory where the PyWin32 .h and .lib files are installed.
 # If you are doing a win32com extension, you will also need to add
 # win32com\Include and win32com\Libs.
 ext = Extension("win32_extension", sources,
-                include_dirs = [os.path.join(get_python_lib(), "win32", "Include")],
-                library_dirs = [os.path.join(get_python_lib(), "win32", "Libs")],
+                include_dirs = [lib_dir, "win32", "include")],
+                library_dirs = [lib_dir, "win32", "libs")],
                 )
 
 setup(

setup.py
@@ -89,8 +89,8 @@ from distutils import log
 static_crt_modules = ["winxpgui"]
 
 
+from sysconfig import get_config_var, get_config_vars
 from distutils.dep_util import newer_group
-from distutils.sysconfig import get_config_vars
 from distutils.filelist import FileList
 from distutils.errors import DistutilsExecError, DistutilsSetupError
 import distutils.util
@@ -1037,7 +1037,7 @@ class my_build_ext(build_ext):
             elif ext.name in ("win32ui",):
                 if sys.version_info >= (3, 10):
                     # Versioned ext DLL filenames in Py3.10+ like 'win32ui.cp310-win_amd64.pyd'
-                    name1 = ext.name + os.path.splitext(distutils.sysconfig.get_config_var('EXT_SUFFIX'))[0] + extra
+                    name1 = ext.name + os.path.splitext(get_config_var('EXT_SUFFIX'))[0] + extra
                     name2 = ext.name + extra
                 else:
                     name1 = name2 = ext.name + extra

Pythonwin/pywin/framework/app.py
@@ -335,8 +335,8 @@ class AboutBox(dialog.Dialog):
 		self.SetDlgItemText(win32ui.IDC_EDIT1, text)
 		# Get the build number - written by installers.
 		# For distutils build, read pywin32.version.txt
-		import distutils.sysconfig
-		site_packages = distutils.sysconfig.get_python_lib(plat_specific=1)
+		import sysconfig
+		site_packages = sysconfig.get_paths()["platlib"]
 		try:
 			build_no = open(os.path.join(site_packages, "pywin32.version.txt")).read().strip()
 			ver = "pywin32 build %s" % build_no

Pythonwin/pywin/framework/mdi_pychecker.py
@@ -299,8 +299,8 @@ class TheDocument(docview.RichEditDoc):
             except win32api.error:
                 py = ""
             # Find checker.py
-            from distutils.sysconfig import get_python_lib
-            pychecker = os.path.join(get_python_lib(), 'pychecker', 'checker.py')
+            import sysconfig
+            pychecker = os.path.join(sysconfig.get_paths()["purelib"], 'pychecker', 'checker.py')
             if not os.path.isfile(py):
                 result = "Can't find python.exe!\n"
             elif not os.path.isfile(pychecker):

pywin32_postinstall.py
@@ -7,7 +7,7 @@ import sys
 import glob
 import shutil
 import time
-import distutils.sysconfig
+import sysconfig
 try:
     import winreg as winreg
 except:
@@ -225,7 +225,7 @@ def RegisterCOMObjects(register=True):
 
 def RegisterHelpFile(register=True, lib_dir=None):
     if lib_dir is None:
-        lib_dir = distutils.sysconfig.get_python_lib(plat_specific=1)
+        lib_dir = sysconfig.get_paths()["platlib"]
     if register:
         # Register the .chm help file.
         chm_file = os.path.join(lib_dir, "PyWin32.chm")
@@ -249,7 +249,7 @@ def RegisterPythonwin(register=True, lib_dir=None):
     import os
 
     if lib_dir is None:
-        lib_dir = distutils.sysconfig.get_python_lib(plat_specific=1)
+        lib_dir = sysconfig.get_paths()["platlib"]
     classes_root=get_root_hkey()
     ## Installer executable doesn't seem to pass anything to postinstall script indicating if it's a debug build,
     pythonwin_exe = os.path.join(lib_dir, "Pythonwin", "Pythonwin.exe")
@@ -656,7 +656,7 @@ if __name__ == '__main__':
                         action='store_true',
                         help="Don't display progress messages.")
     parser.add_argument("-destination",
-                        default=distutils.sysconfig.get_python_lib(plat_specific=1),
+                        default=sysconfig.get_paths()["platlib"],
                         type=verify_destination,
                         help="Location of the PyWin32 installation")
 
@@ -671,9 +671,6 @@ if __name__ == '__main__':
     if args.wait is not None:
         try:
             os.waitpid(args.wait, 0)
-        except AttributeError:
-            # Python 2.2 - no waitpid - just sleep.
-            time.sleep(3)
         except os.error:
             # child already dead
             pass

win32/Lib/pywintypes.py
@@ -77,8 +77,8 @@ def __import_pywin32_system_module__(modname, globs):
             # This isn't ideal as it means, say 'python -c "import win32api"'
             # will not work but 'python -c "import pywintypes, win32api"' will,
             # but it's better than nothing...
-            import distutils.sysconfig
-            maybe = os.path.join(distutils.sysconfig.get_python_lib(plat_specific=1),
+            import sysconfig
+            maybe = os.path.join(sysconfig.get_paths()["platlib"],
                                  "pywin32_system32", filename)
             if os.path.isfile(maybe):
                 found = maybe

commit ad2a479be481d04ae0fb01bb7803ada66a2942d3

fix regression: aa30f7586 broke the demo setup.py

win32/Demos/c_extension/setup.py
@@ -7,18 +7,18 @@ from distutils.core import setup, Extension
 from sysconfig import get_paths
 
 sources = ["win32_extension.cpp"]
-libdir = sysconfig.get_paths()["platlib"]
+lib_dir = get_paths()["platlib"]
 
 # Specify the directory where the PyWin32 .h and .lib files are installed.
 # If you are doing a win32com extension, you will also need to add
 # win32com\Include and win32com\Libs.
 ext = Extension("win32_extension", sources,
-                include_dirs = [lib_dir, "win32", "include")],
-                library_dirs = [lib_dir, "win32", "libs")],
+                include_dirs = [os.path.join(lib_dir, "win32", "include")],
+                library_dirs = [os.path.join(lib_dir, "win32", "libs")],
                 )
 
 setup(
-    name="win32 extension sample", 
+    name="win32 extension sample",
     version="0.1",
     ext_modules=[ext],
 )