4baccf3149ce56983e2068e25ec062ed15dcc1ad
Mark Hammond <mhammond@skippinet.com.au>
2021-12-19 20:38:15 +1100

    oops - bdist_wininst needs --target-version

--- make_all.bat
+++ make_all.bat
@@ -12,24 +12,25 @@ cd ..
 :already_built
 rem Now the binaries.
 
-py -3.6-32 setup.py -q bdist_wininst --skip-build
+rem (bdist_wininst needs --target-version to name the installers correctly!)
+py -3.6-32 setup.py -q bdist_wininst --skip-build --target-version=3.6
 py -3.6-32 setup.py -q bdist_wheel --skip-build
-py -3.6 setup.py -q bdist_wininst --skip-build
+py -3.6 setup.py -q bdist_wininst --skip-build --target-version=3.6
 py -3.6 setup.py -q bdist_wheel --skip-build
 
-py -3.7-32 setup.py -q bdist_wininst --skip-build
+py -3.7-32 setup.py -q bdist_wininst --skip-build --target-version=3.7
 py -3.7-32 setup.py -q bdist_wheel --skip-build
-py -3.7 setup.py -q bdist_wininst --skip-build
+py -3.7 setup.py -q bdist_wininst --skip-build --target-version=3.7
 py -3.7 setup.py -q bdist_wheel --skip-build
 
-py -3.8-32 setup.py -q bdist_wininst --skip-build
+py -3.8-32 setup.py -q bdist_wininst --skip-build --target-version=3.8
 py -3.8-32 setup.py -q bdist_wheel --skip-build
-py -3.8 setup.py -q bdist_wininst --skip-build
+py -3.8 setup.py -q bdist_wininst --skip-build --target-version=3.8
 py -3.8 setup.py -q bdist_wheel --skip-build
 
-py -3.9-32 setup.py -q bdist_wininst --skip-build
+py -3.9-32 setup.py -q bdist_wininst --skip-build --target-version=3.9
 py -3.9-32 setup.py -q bdist_wheel --skip-build
-py -3.9 setup.py -q bdist_wininst --skip-build
+py -3.9 setup.py -q bdist_wininst --skip-build --target-version=3.9
 py -3.9 setup.py -q bdist_wheel --skip-build
 
 rem 3.10 stopped supporting bdist_wininst, but we can still build them with 3.9
