981f09b1f95a5c222b174c850ef65ca56e7305cb
Mark Hammond <mhammond@skippinet.com.au>
2022-01-02 13:59:00 +1100

    Don't import 2to3 hacks in adodbapi's setup.py (#1816)

--- adodbapi/setup.py
+++ adodbapi/setup.py
@@ -1,8 +1,7 @@
 """adodbapi -- a pure Python PEP 249 DB-API package using Microsoft ADO
 
-Adodbapi can be run on CPython version 2.7,
-or IronPython version 2.6 and later,
-or Python 3.5 and later (after filtering through 2to3.py)
+Adodbapi can be run on CPython 3.5 and later.
+or IronPython version 2.6 and later (in theory, possibly no longer in practice!)
 """
 CLASSIFIERS = """\
 Development Status :: 5 - Production/Stable
@@ -40,31 +39,13 @@ for line in a:
         break
 a.close()
 
-##DOWNLOAD_URL = "http://sourceforge.net/projects/adodbapi/files/adodbapi/" + VERSION.rsplit('.', 1)[0] + '/adodbapi-' + VERSION + '.zip'
-
 import sys
 
 
 def setup_package():
 
     from distutils.core import setup
-
-    if sys.version_info >= (3, 0):
-
-        try:
-            from distutils.command.build_py import build_py_2to3 as build_py
-        ##        # exclude fixers that break already compatible code
-        ##        from lib2to3.refactor import get_fixers_from_package
-        ##        fixers = get_fixers_from_package('lib2to3.fixes')
-        ##        for skip_fixer in ['import']:
-        ##            fixers.remove('lib2to3.fixes.fix_' + skip_fixer)
-        ##        build_py.fixer_names = fixers
-        except ImportError:
-            raise ImportError(
-                "build_py_2to3 not found in distutils - it is required for Python 3.x"
-            )
-    else:
-        from distutils.command.build_py import build_py
+    from distutils.command.build_py import build_py
 
     setup(
         cmdclass={"build_py": build_py},
