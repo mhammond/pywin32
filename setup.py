4683ff4d6412554c775e64736e92a7e558104689
Andreas Maier <andreas.r.maier@gmx.de>
2023-03-11 17:08:47 +0100

    Added supported Python versions to package metadata
    Details:
    
    * Added the 'python_requires' argument to setup() to specify the supported
      Python versions in the package metadata. Installers such as pip depend
      on that, and there are issues such as #1448 when the installer does not
      have that information in the package.
    
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- setup.py
+++ setup.py
@@ -2249,6 +2249,8 @@ classifiers = [
     "Programming Language :: Python :: Implementation :: CPython",
 ]
 
+python_requires = ">=3.7"
+
 if "bdist_wininst" in sys.argv:
     # fixup https://github.com/pypa/setuptools/issues/3284
     def maybe_fixup_exes():
@@ -2301,6 +2303,7 @@ dist = setup(
     url="https://github.com/mhammond/pywin32",
     license="PSF",
     classifiers=classifiers,
+    python_requires=python_requires,
     cmdclass=cmdclass,
     options={
         "bdist_wininst": {
