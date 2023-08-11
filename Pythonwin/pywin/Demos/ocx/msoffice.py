57996d7d82ad3b936e97df199e9ea0ec8c5cffcc
Avasam <samuel.06@hotmail.com>
2023-08-11 11:06:55 -0400

    Fix more demos

--- Pythonwin/pywin/Demos/ocx/msoffice.py
+++ Pythonwin/pywin/Demos/ocx/msoffice.py
@@ -11,10 +11,6 @@ import win32uiole
 from pywin.mfc import activex, docview, object, window
 from win32com.client import gencache
 
-# WordModule = gencache.EnsureModule('{00020905-0000-0000-C000-000000000046}', 1033, 8, 0)
-# if WordModule is None:
-# 	raise ImportError, "Microsoft Word version 8 does not appear to be installed."
-
 
 class OleClientItem(object.CmdTarget):
     def __init__(self, doc):
@@ -123,6 +119,18 @@ class WordFrame(window.MDIChildWnd):
         # Dont call base class doc/view version...
 
     def Create(self, title, rect=None, parent=None):
+        WordModule = gencache.EnsureModule(
+            "{00020905-0000-0000-C000-000000000046}", 1033, 8, 0
+        )
+        if WordModule is None:
+            raise ImportError(
+                "Microsoft Word version 8 does not appear to be installed."
+            )
+
+        # WordModule.Word doesn't exist in WordModule, WordModule.Words does, but CreateControl still fails
+        class MyWordControl(activex.Control, WordModule.Word):
+            ...
+
         style = win32con.WS_CHILD | win32con.WS_VISIBLE | win32con.WS_OVERLAPPEDWINDOW
         self._obj_.CreateWindow(None, title, style, rect, parent)
 
@@ -142,11 +150,12 @@ def Demo():
     docName = None
     if len(sys.argv) > 1:
         docName = win32api.GetFullPathName(sys.argv[1])
-    OleTemplate().OpenDocumentFile(None)
+    OleTemplate().OpenDocumentFile(docName)
 
+    # ActiveX not currently working
+    # f = WordFrame(docName)
+    # f.Create("Microsoft Office")
 
-# 	f = WordFrame(docName)
-# 	f.Create("Microsoft Office")
 
 if __name__ == "__main__":
     Demo()
