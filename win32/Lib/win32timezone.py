be6efa229ced09a37e760cf71367461bf3dd88b5
Avasam <samuel.06@hotmail.com>
2023-08-22 17:33:57 -0400

    Update unicode strings in comments

--- win32/Lib/win32timezone.py
+++ win32/Lib/win32timezone.py
@@ -144,7 +144,7 @@ True
 True
 
 This test helps ensure language support for unicode characters
->>> x = TIME_ZONE_INFORMATION(0, u'français')
+>>> x = TIME_ZONE_INFORMATION(0, 'français')
 
 
 Test conversion from one time zone to another at a DST boundary
