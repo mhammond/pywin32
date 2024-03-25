d457a63c0374dadfcb1e8d9e5a7d2ad7601229b2
Avasam <samuel.06@hotmail.com>
2024-03-25 11:54:22 -0400

    Run checkers

--- win32/winxpgui.py
+++ win32/winxpgui.py
@@ -2,6 +2,7 @@
 The `winxpgui` module is obsolete and has been completely replaced \
 by `win32gui` and `win32console.GetConsoleWindow`. Use those instead. \
 """
+
 from __future__ import annotations
 
 import warnings
@@ -9,7 +10,8 @@ import warnings
 from win32console import GetConsoleWindow as GetConsoleWindow
 from win32gui import *
 
-warnings.warn("""\
+warnings.warn(
+    """\
 The `winxpgui` module is obsolete and has been completely replaced \
 by `win32gui` and `win32console.GetConsoleWindow`. Use those instead. \
 """,
