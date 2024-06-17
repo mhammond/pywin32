cf8fb2be5eb3ffe72690fe7636b4d2f23d6b9788
Charlie Lin <tuug@gmx.us>
2024-06-17 09:56:25 -0400

    Advertise 3.13 compatibility for PyPI

--- setup.py
+++ setup.py
@@ -2207,6 +2207,7 @@ classifiers = [
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
     "Programming Language :: Python :: Implementation :: CPython",
 ]
 
