7f04a7cc4f979d51d6a33ea541096e443fb5e3e3
Avasam <samuel.06@hotmail.com>
2024-11-18 00:23:59 -0500

    Vendor h2py to regenerate scintillacon.py

--- setup.py
+++ setup.py
@@ -453,7 +453,7 @@ class my_build_ext(build_ext):
             assert os.path.isdir(build_temp), build_temp
         makeargs.append("SUB_DIR_O=%s" % build_temp)
         makeargs.append("SUB_DIR_BIN=%s" % build_temp)
-        makeargs.append("DIR_PYTHON=%s" % sys.prefix)
+        makeargs.append(f'PYTHON="{sys.executable}"')
 
         nmake = "nmake.exe"
         # Attempt to resolve nmake to the same one that our compiler object
