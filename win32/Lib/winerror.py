4f7fb57b2ee2684c48d849d43bc33bf696a595e5
Avasam <samuel.06@hotmail.com>
2024-12-31 19:38:57 -0500

    Remove TODO

--- win32/Lib/winerror.py
+++ win32/Lib/winerror.py
@@ -1,5 +1,3 @@
-# pyright: reportConstantRedefinition=false
-# TODO: Fix in its own PR be regenerating this module
 """Error related constants for win32
 
 Generated from winerror.h and cderror.h"""
