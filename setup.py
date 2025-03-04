b88e7342384c880d8b3e45b0e56a58eedfa04acf
Avasam <samuel.06@hotmail.com>
2025-03-03 23:45:54 -0500

    Add # type: ignore[override]

--- setup.py
+++ setup.py
@@ -964,7 +964,7 @@ class my_compiler(MSVCCompiler):
         sources = sorted(sources, key=key_reverse_mc)
         return MSVCCompiler.compile(self, sources, **kwargs)
 
-    def spawn(self, cmd: MutableSequence[str]) -> None:
+    def spawn(self, cmd: MutableSequence[str]) -> None:  # type: ignore[override] # More restrictive than supertype
         is_link = cmd[0].endswith("link.exe") or cmd[0].endswith('"link.exe"')
         is_mt = cmd[0].endswith("mt.exe") or cmd[0].endswith('"mt.exe"')
         if is_mt:
