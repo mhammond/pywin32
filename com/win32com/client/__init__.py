b0e512fcc1cd3e57cc60db8ada2e01ac4b743695
Florian Demski <mail@florian-demski.de>
2025-04-13 21:35:38 +0200

    reduced duplicates, changelog

--- com/win32com/client/__init__.py
+++ com/win32com/client/__init__.py
@@ -524,25 +524,10 @@ class DispatchBaseClass:
     def __init__(self, oobj=None):
         if oobj is None:
             oobj = pythoncom.new(self.CLSID)
-        elif isinstance(oobj, DispatchBaseClass):
+        elif isinstance(oobj, (DispatchBaseClass, _PyIDispatchType)):
             try:
-                oobj = oobj._oleobj_.QueryInterface(
-                    self.CLSID, pythoncom.IID_IDispatch
-                )  # Must be a valid COM instance
-            except pythoncom.com_error as details:
-                import winerror
-
-                # Some stupid objects fail here, even tho it is _already_ IDispatch!!??
-                # Eg, Lotus notes.
-                # So just let it use the existing object if E_NOINTERFACE
-                if details.hresult != winerror.E_NOINTERFACE:
-                    raise
-                oobj = oobj._oleobj_
-        elif isinstance(oobj, _PyIDispatchType):
-            try:
-                oobj = oobj.QueryInterface(
-                    self.CLSID, pythoncom.IID_IDispatch
-                )  # Must be a valid COM instance
+                oobj = oobj._oleobj_ if isinstance(oobj, DispatchBaseClass) else oobj
+                oobj = oobj.QueryInterface(self.CLSID, pythoncom.IID_IDispatch)
             except pythoncom.com_error as details:
                 import winerror
 
