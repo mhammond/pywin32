3c35b484cde1ba9201f2159bdafd8e88a42ce89c
day kay <12646261+Devel-Dan@users.noreply.github.com>
2025-05-04 15:16:44 -0700

    Update win32/Lib/pywin32_bootstrap.py
    Co-authored-by: Avasam <samuel.06@hotmail.com>

--- win32/Lib/pywin32_bootstrap.py
+++ win32/Lib/pywin32_bootstrap.py
@@ -17,18 +17,19 @@ else:
     # https://docs.python.org/3/reference/import.html#path-attributes-on-modules
     for path in pywin32_system32.__path__:
         if os.path.isdir(path):
-            try:
-                # First try the preferred method
+            # First try the preferred method
+            if hasattr(os, "add_dll_directory"):
                 os.add_dll_directory(path)
-            except Exception:
-                # If anything fails, try to modify PATH if it exists
-                try:
-                    if "PATH" in os.environ:
-                        os.environ["PATH"] = path + os.pathsep + os.environ["PATH"]
-                    else:
-                        # If PATH doesn't exist, just create it
-                        os.environ["PATH"] = path
-                except Exception:
-                    # Last resort - if nothing works, just pass silently
-                    pass
+            # If `add_dll_directory` is missing, which can happen in Pylance early initialization,
+            # try to modify PATH if it exists (just create it if it doesn't)
+            elif "PATH" not in os.environ:
+                os.environ["PATH"] = path
+            else:
+                # This is to ensure the pywin32 path is in the beginning to find the
+                # pywin32 DLLs first and prevent other PATH entries to shadow them
+                prepend_to_path = path + os.pathsep
+                if not os.environ["PATH"].startswith(prepend_to_path):
+                    os.environ["PATH"] = prepend_to_path + os.environ["PATH"].replace(
+                        os.pathsep + path, ""
+                    )
             break
