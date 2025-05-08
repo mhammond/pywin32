57e157b003b7628a83f17b5e2002a5fdca90e94d
Avasam <samuel.06@hotmail.com>
2025-05-07 20:28:23 -0400

    Move ps script to a python script

---
+++ .github/workflows/install-vs-components.py
@@ -0,0 +1,37 @@
+import os
+import platform
+from subprocess import check_call, check_output
+
+os.chdir("C:/Program Files (x86)/Microsoft Visual Studio/Installer")
+vs_install_path = check_output(
+    (
+        "vswhere.exe",
+        "-latest",
+        "-products",
+        "*",
+        "-requires",
+        "Microsoft.Component.MSBuild",
+        "-property",
+        "installationPath",
+    ),
+    text=True,
+)
+components_to_add = (
+    ["Microsoft.VisualStudio.Component.VC.14.29.16.11.ATL.ARM64"]
+    if platform.machine() == "ARM64"
+    else ["Microsoft.VisualStudio.Component.VC.14.29.16.11.ATL"]
+)
+args = (
+    "vs_installer.exe",
+    "modify",
+    "--installPath",
+    vs_install_path,
+    *[f"--add {component}" for component in components_to_add],
+    "--quiet",
+    "--norestart",
+    "--nocache",
+)
+
+# Should be run twice for some reason
+check_call(args)
+check_call(args)
