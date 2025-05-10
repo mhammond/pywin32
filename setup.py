aedcff915c1ac82f2a5096f677008fef44828137
Avasam <samuel.06@hotmail.com>
2025-05-10 11:37:22 -0400

    Temporary workaround for Python 3.14 generated headers

--- setup.py
+++ setup.py
@@ -134,6 +134,9 @@ class WinExt(Extension):
                 # https://learn.microsoft.com/en-us/cpp/porting/modifying-winver-and-win32-winnt
                 ("_WIN32_WINNT", hex(0x0601)),
                 ("WINVER", hex(0x0601)),
+                # Temporary workaround for Python 3.14 generated headers
+                # https://github.com/python/cpython/issues/133779#issuecomment-2867685033
+                ("Py_GIL_DISABLED", 0),
             )
         )
         self.pch_header = pch_header
