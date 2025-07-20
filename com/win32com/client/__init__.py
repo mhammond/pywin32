mhammond/pywin32

commit 25e01da89dbec0da9180e33f554f4841c2599551

Fix dispatch handling for properties (#2506)

com/win32com/client/__init__.py
@@ -524,11 +524,10 @@ class DispatchBaseClass:
     def __init__(self, oobj=None):
         if oobj is None:
             oobj = pythoncom.new(self.CLSID)
-        elif isinstance(oobj, DispatchBaseClass):
+        elif isinstance(oobj, (DispatchBaseClass, _PyIDispatchType)):
             try:
-                oobj = oobj._oleobj_.QueryInterface(
-                    self.CLSID, pythoncom.IID_IDispatch
-                )  # Must be a valid COM instance
+                oobj = oobj._oleobj_ if isinstance(oobj, DispatchBaseClass) else oobj
+                oobj = oobj.QueryInterface(self.CLSID, pythoncom.IID_IDispatch)
             except pythoncom.com_error as details:
                 import winerror
 
@@ -537,7 +536,7 @@ class DispatchBaseClass:
                 # So just let it use the existing object if E_NOINTERFACE
                 if details.hresult != winerror.E_NOINTERFACE:
                     raise
-                oobj = oobj._oleobj_
+
         self.__dict__["_oleobj_"] = oobj  # so we don't call __setattr__
 
     def __dir__(self):

commit 952953d3f78cda610d124a2523eccf9ffd14386e

Fix regression introduced by PR #2506

com/win32com/client/__init__.py
@@ -529,13 +529,14 @@ class DispatchBaseClass:
                 oobj = oobj._oleobj_ if isinstance(oobj, DispatchBaseClass) else oobj
                 oobj = oobj.QueryInterface(self.CLSID, pythoncom.IID_IDispatch)
             except pythoncom.com_error as details:
-                import winerror
+                if not isinstance(oobj, _PyIDispatchType):
+                    import winerror
 
-                # Some stupid objects fail here, even tho it is _already_ IDispatch!!??
-                # Eg, Lotus notes.
-                # So just let it use the existing object if E_NOINTERFACE
-                if details.hresult != winerror.E_NOINTERFACE:
-                    raise
+                    # Some stupid objects fail here, even tho it is _already_ IDispatch!!??
+                    # Eg, Lotus notes.
+                    # So just let it use the existing object if E_NOINTERFACE
+                    if details.hresult != winerror.E_NOINTERFACE:
+                        raise
 
         self.__dict__["_oleobj_"] = oobj  # so we don't call __setattr__
 