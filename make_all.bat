86757ea337c9f59963106cbcee8b7b33d656a086
Avasam <samuel.06@hotmail.com>
2026-05-11 12:34:40 -0400

    each iteration on their own lines

--- make_all.bat
+++ make_all.bat
@@ -7,14 +7,29 @@ py autoduck\make.py
 
 rem Check /build_env.md#build-environment to make sure you have all the required components installed
 @echo off
-for %%V in (3.9 3.10 3.11 3.12 3.13 3.14 3.15) do (
+for %%V in (
+    3.9
+    3.10
+    3.11
+    3.12
+    3.13
+    3.14
+    3.15
+) do (
     py -%%V-32 -m build --wheel
     py -%%V -m build --wheel
 )
 
 rem Check /build_env.md#cross-compiling-for-arm64-microsoft-visual-c-141-and-up to make sure you have all the required ARM64 components installed
 @echo off
-for %%V in (3.10 3.11 3.12 3.13 3.14 3.15) do (
+for %%V in (
+    3.10
+    3.11
+    3.12
+    3.13
+    3.14
+    3.15
+) do (
     py -%%V -m build --wheel --config-setting=--build-option="build_ext --plat-name=win-arm64 build --plat-name=win-arm64 bdist_wheel --plat-name=win-arm64"
 )
 
