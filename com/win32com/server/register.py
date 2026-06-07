1ea0a58aa052d655e373701c0dcb48ba17f99aa7
Charlie Lin <tuug@gmx.us>
2026-06-07 11:16:54 -0400

    Use pythonw again

--- com/win32com/server/register.py
+++ com/win32com/server/register.py
@@ -103,9 +103,9 @@ def _find_localserver_exe(mustfind):
     if sys.platform != "win32":
         return sys.executable
     if os.path.splitext(os.path.basename(pythoncom.__file__))[0].endswith("_d"):
-        exeBaseName = "python_d.exe"
+        exeBaseName = "pythonw_d.exe"
     else:
-        exeBaseName = "python.exe"
+        exeBaseName = "pythonw.exe"
     # First see if in the same directory as this .EXE
     exeName = os.path.join(os.path.split(sys.executable)[0], exeBaseName)
     if not os.path.exists(exeName):
