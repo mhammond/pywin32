0ab8c961c6f63af3f4e70af84bb4e547b82601c6
Charlie Lin <tuug@gmx.us>
2026-06-07 10:21:40 -0400

    Update testPyComTest.py

--- com/win32com/test/testPyComTest.py
+++ com/win32com/test/testPyComTest.py
@@ -31,7 +31,6 @@ from win32process import GetProcessMemoryInfo
 win32com.test.util.RegisterPythonServer(
     os.path.join(os.path.dirname(__file__), "..", "servers", "test_pycomtest.py"),
     "Python.Test.PyCOMTest",
-    True,
 )
 
 try:
